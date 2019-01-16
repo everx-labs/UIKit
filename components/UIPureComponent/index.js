ef54002d6c7e2f14a1ffc9906c42567252476103
Alexander <aldelank@gmail.com>
2019-01-16 22:40:47 +0300

    add UIPureComponent

---
+++ components/UIPureComponent/index.js
@@ -0,0 +1,28 @@
+// @flow
+import React, { PureComponent } from 'react';
+
+type StateUpdate<State, Props> = ((State, Props) => $Shape<State> | void);
+
+export default class UIPureComponent<Props, State> extends PureComponent<Props, State> {
+    componentDidMount() {
+        this.mounted = true;
+    }
+
+    componentWillUnmount() {
+        this.mounted = false;
+    }
+
+    setStateSafely(
+        state: $Shape<State> | StateUpdate,
+        callback?: () => mixed,
+    ) {
+        if (!this.mounted) {
+            return;
+        }
+        this.setState(state, callback);
+    }
+
+    render() {
+        return null;
+    }
+}
