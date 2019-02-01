8f5d2dbf133c091248dc79d921db1f195a0973a7
Jose Cruz Marquez Aguilar <jose@cruzmarquez.com>
2019-02-01 19:57:59 +0500

    Fixed to return a promise and a value in case of an error

--- helpers/UIDevice/index.js
+++ helpers/UIDevice/index.js
@@ -31,7 +31,14 @@ export default class UIDevice {
 
     static safeAreaInsets() {
         return SafeArea.getSafeAreaInsetsForRootView().then((result) => {
-            return result.safeAreaInsets;
+            return Promise.resolve(result.safeAreaInsets);
+        }).catch(() => {
+            return Promise.resolve({
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+            });
         });
     }
 
