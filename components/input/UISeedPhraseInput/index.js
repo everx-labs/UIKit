23eeb2335dc13c83137a079fa392f5ac497c5fae
Jose <8920394+jcma86@users.noreply.github.com>
2019-07-15 18:43:06 +0300

    Update index.js

--- components/input/UISeedPhraseInput/index.js
+++ components/input/UISeedPhraseInput/index.js
@@ -51,7 +51,8 @@ export default class UISeedPhraseInput extends UIDetailsInput<Props, State> {
     };
 
     static splitPhrase(phrase: string): Array<string> {
-        const noExtraSpaces = phrase.replace(/\s+/g, ' ');
+        const noRegularDash = phrase.replace(/-+/g, ` ${UIConstant.dashSymbol()} `);
+	    const noExtraSpaces = noRegularDash.replace(/\s+/g, ' ');
         const words = noExtraSpaces.split(' ');
         const normalized = [];
         for (let i = 0; i < words.length; i += 1) {
