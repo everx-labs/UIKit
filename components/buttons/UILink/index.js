aea0f65038d3b9bfc15288781c6ab8840b01ca03
Dilnara Kasymova <dilnara.kasymova@gmail.com>
2019-08-15 01:53:36 +0700

    remove unnecessary

--- components/buttons/UILink/index.js
+++ components/buttons/UILink/index.js
@@ -1,12 +1,11 @@
 // @flow
 import React from 'react';
 import StylePropType from 'react-style-proptype';
-import { StyleSheet, View, Text, Image, Animated, Easing } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import UIButton from '../UIButton';
 import UIComponent from '../../UIComponent';
 import UIColor from '../../../helpers/UIColor';
-import UIConstant from '../../../helpers/UIConstant';
 import UIStyle from '../../../helpers/UIStyle';
 
 const styles = StyleSheet.create({
@@ -45,8 +44,6 @@ type Props = {
     /** @ignore */
     theme?: string,
 
-
-    // parent's props for stupid docgen which doesn't see them.
     /** set true if button disabled */
     disabled?: boolean,
     /** set true for indicating wait state */
@@ -62,39 +59,7 @@ type Props = {
 type State = ActionState;
 
 export default class UILink extends UIComponent<Props, State> {
-    static LinkSize = {
-        Large: 'large',
-        Medium: 'medium',
-        Small: 'small',
-    };
-
-    static TextAlign = {
-        Center: 'center',
-        Left: 'left',
-        Right: 'right',
-    };
-
-    static Indicator = {
-        Spin: 'spin',
-        Round: 'round',
-        Sandglass: 'sandglass',
-        Pulse: 'pulse',
-    };
-
-    constructor(props: Props) {
-        super(props);
-    }
-
     render() {
-        const {
-            style, textAlign,
-            icon, iconR, title,
-            hasIcon,
-            hasIconR,
-            showIndicator,
-            buttonSize,
-        } = this.props;
-
         return (
             <UIButton {...this.props} buttonStyle={UIButton.ButtonStyle.Link} />
         );
