787df0a7401b68306a2374a2db03f3d7b9ad61c9
Dilnara Kasymova <dilnara.kasymova@gmail.com>
2019-09-03 16:42:31 +0700

    review fixes

--- components/storybook/stories/buttonGroup.stories.js
+++ components/storybook/stories/buttonGroup.stories.js
@@ -16,6 +16,9 @@ import {
 
 const iconDefault = getUri(require('../../../assets/ico-triangle/ico-triangle.png'), 24, 24);
 
+const getFlexStyle = (flex) => {
+    return { flex };
+};
 
 storiesOf(Constants.CategoryButtonGroup, module)
     .addParameters({
@@ -28,83 +31,83 @@ storiesOf(Constants.CategoryButtonGroup, module)
     .add('Row', () => (
         <React.Fragment>
             <UIButtonGroup>
-                <UIButton title="Action" style={{ flex: 1 }} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Accent Action" style={{ flex: 2 }} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Accent Action" style={getFlexStyle(2)} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Loooooooong Action" style={{ flex: 3 }} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Loooooooong Action" style={getFlexStyle(3)} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Action" style={{ flex: 1 }} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Accent Action" style={{ flex: 2 }} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Accent Action" style={getFlexStyle(2)} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Action" style={{ flex: 2 }} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIActionIcon icon={iconDefault} style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(2)} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIActionIcon icon={iconDefault} style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Accent Action" style={{ flex: 2 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Accent Action" style={getFlexStyle(2)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Loooooooong Action" style={{ flex: 3 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Loooooooong Action" style={getFlexStyle(3)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Accent Action" style={{ flex: 2 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Accent Action" style={getFlexStyle(2)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Action" style={{ flex: 2 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIActionIcon icon={iconDefault} style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(2)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIActionIcon icon={iconDefault} style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
             </UIButtonGroup>
 
             <UIButtonGroup style={UIStyle.Margin.topDefault()}>
-                <UIButton title="Back" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Border} />
-                <UIButton title="Accent Action" style={{ flex: 2 }} />
+                <UIButton title="Back" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Border} />
+                <UIButton title="Accent Action" style={getFlexStyle(2)} />
             </UIButtonGroup>
         </React.Fragment>
     ))
     .add('Stacked', () => (
         <React.Fragment>
             <UIButtonGroup direction={UIButtonGroup.Direction.Column} gutter={0}>
-                <UIButton title="Loooooooong Action" style={{ flex: 1 }} />
-                <UIButton title="Loooooooong Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Link} />
-                <UIButton title="Loooooooong Action" style={{ flex: 1 }} buttonStyle={UIButton.ButtonStyle.Border} />
+                <UIButton title="Loooooooong Action" style={getFlexStyle(1)} />
+                <UIButton title="Loooooooong Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Link} />
+                <UIButton title="Loooooooong Action" style={getFlexStyle(1)} buttonStyle={UIButton.ButtonStyle.Border} />
             </UIButtonGroup>
         </React.Fragment>
     ));
--- components/storybook/stories/grid.stories.js
+++ components/storybook/stories/grid.stories.js
@@ -13,33 +13,34 @@ import {
 } from '../../../UIKit';
 
 const styles = {
-    grid: {
-        backgroundColor: UIColor.whiteLight(),
-        marginTop: 16,
-    },
+    grid: [
+        UIStyle.Color.getBackgroundColorStyle(UIColor.whiteLight()),
+        UIStyle.Margin.topDefault(),
+    ],
+    container: { height: 500 },
 };
 
 storiesOf(Constants.CategoryLayout, module)
     .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
     .add('Grid', () => (
-        <View style={{ height: 500 }}>
+        <View style={styles.container}>
             <UIGrid
                 type={UIGrid.Type.C6}
                 style={styles.grid}
             >
-                <View style={{ backgroundColor: UIColor.success() }} medium={3}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={3}>
                     <UIButton title="3 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.warning() }} medium={3}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.warning())} medium={3}>
                     <UIButton title="3 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
             </UIGrid>
@@ -48,22 +49,22 @@ storiesOf(Constants.CategoryLayout, module)
                 type={UIGrid.Type.C8}
                 style={styles.grid}
             >
-                <View style={{ backgroundColor: UIColor.warning() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.warning())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={2}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={2}>
                     <UIButton title="2 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.warning() }} medium={4}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.warning())} medium={4}>
                     <UIButton title="4 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.warning() }} medium={3}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.warning())} medium={3}>
                     <UIButton title="3 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.warning() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.warning())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
             </UIGrid>
@@ -74,49 +75,49 @@ storiesOf(Constants.CategoryLayout, module)
                 gutter={4}
                 rowGutter={8}
             >
-                <View style={{ backgroundColor: UIColor.success() }} medium={4}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={4}>
                     <UIButton title="4 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={4}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={4}>
                     <UIButton title="4 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={4}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={4}>
                     <UIButton title="4 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
-                <View style={{ backgroundColor: UIColor.success() }} medium={1}>
+                <View style={UIStyle.Color.getBackgroundColorStyle(UIColor.success())} medium={1}>
                     <UIButton title="1 cells" buttonStyle={UIButton.ButtonStyle.Border} />
                 </View>
             </UIGrid>
