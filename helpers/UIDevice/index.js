43ac8072a5543ce7262e0e93b68be0586aeda0db
sertony <serkov_anton@mail.ru>
2019-10-01 16:46:26 +0300

    update UIDevice to get rid of UIWebView

--- helpers/UIDevice/index.js
+++ helpers/UIDevice/index.js
@@ -1,8 +1,16 @@
+// @flow
 import { Platform } from 'react-native';
 import SafeArea from 'react-native-safe-area';
 import MobileDetect from 'mobile-detect';
 import DeviceInfo from 'react-native-device-info';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+type SafeAreaInsets = {
+    top: number,
+    left: number,
+    right: number,
+    bottom: number,
+};
 
 const UI_STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? getStatusBarHeight() : 0;
 const UI_NAVIGATION_BAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
@@ -16,20 +24,28 @@ const UI_IS_MOBILE_WEB = Platform.OS === 'web'
 const UI_IS_TABLET_WEB = Platform.OS === 'web'
         && (new MobileDetect(window.navigator.userAgent)).tablet();
 
-const UI_IS_TABLET = DeviceInfo.isTablet() || UI_IS_TABLET_WEB;
+const UI_IS_TABLET = Platform.OS !== 'web' ? DeviceInfo.isTabletSync() : UI_IS_TABLET_WEB;
 
-const UI_IS_MOBILE = (Platform.OS !== 'web' && !DeviceInfo.isTablet()) || UI_IS_MOBILE_WEB;
+const UI_IS_MOBILE = Platform.OS !== 'web' ? !DeviceInfo.isTabletSync() : UI_IS_MOBILE_WEB;
+
+const APP_NAME = Platform.OS !== 'web' ? DeviceInfo.getApplicationNameSync() : '';
+
+const APP_VERSION = Platform.OS !== 'web' ? DeviceInfo.getReadableVersionSync() : '';
+
+const DEVICE_MODEL = Platform.OS !== 'web' ? DeviceInfo.getModelSync() : 'Browser'; // TODO:
+
+const SYSTEM_VERSION = Platform.OS !== 'web' ? DeviceInfo.getSystemVersionSync() : 'Web'; // TODO:
 
 export default class UIDevice {
-    static statusBarHeight() {
+    static statusBarHeight(): number {
         return UI_STATUS_BAR_HEIGHT;
     }
 
-    static navigationBarHeight() {
+    static navigationBarHeight(): number {
         return UI_NAVIGATION_BAR_HEIGHT;
     }
 
-    static async safeAreaInsets() {
+    static async safeAreaInsets(): Promise<SafeAreaInsets> {
         if (SafeArea.getSafeAreaInsetsForRootView) {
             try {
                 const { safeAreaInsets } = await SafeArea.getSafeAreaInsetsForRootView();
@@ -41,42 +57,48 @@ export default class UIDevice {
         return {
             top: 0,
             left: 0,
-            bottom: 0,
             right: 0,
+            bottom: 0,
         };
     }
 
-    static isDesktopWeb() {
+    static isDesktopWeb(): boolean {
         return UI_IS_DESKTOP_WEB;
     }
 
-    static isMobileWeb() {
+    static isMobileWeb(): boolean {
         return UI_IS_MOBILE_WEB;
     }
 
-    static isTabletWeb() {
+    static isTabletWeb(): boolean {
         return UI_IS_TABLET_WEB;
     }
 
-    static isDesktop() {
+    static isDesktop(): boolean {
         return UI_IS_DESKTOP_WEB; // TODO:
     }
 
-    static isTablet() {
+    static isTablet(): boolean {
         return UI_IS_TABLET;
     }
 
-    static isMobile() {
+    static isMobile(): boolean {
         return UI_IS_MOBILE;
     }
 
     static appName(): string {
-        return Platform.OS === 'web' ? null : DeviceInfo.getApplicationName();
+        return APP_NAME;
     }
 
     static appVersion(): string {
-        return Platform.OS === 'web' ?
-            null
-            : `${DeviceInfo.getVersion()}.${DeviceInfo.getBuildNumber()}`;
+        return APP_VERSION;
+    }
+
+    static deviceModel(): string {
+        return DEVICE_MODEL;
+    }
+
+    static systemVersion(): string {
+        return SYSTEM_VERSION;
     }
 }
