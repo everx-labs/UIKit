6a85a2334c1243331c55509b19db0285f56cc0f0
Dilnara Kasymova <dilnara.kasymova@gmail.com>
2019-10-03 02:18:37 +0700

    fix UILink, don't duplicate props

--- components/buttons/UILink/index.js
+++ components/buttons/UILink/index.js
@@ -8,93 +8,19 @@ import UIComponent from '../../UIComponent';
 import UIColor from '../../../helpers/UIColor';
 import UIStyle from '../../../helpers/UIStyle';
 
+import type { ButtonProps } from '../UIButton';
+
 const styles = StyleSheet.create({
     container: {
         overflow: 'hidden',
     },
 });
 
-type Props = {
-    /** One of:
-    UIButton.ButtonSize.Large,
-    UIButton.ButtonSize.Medium,
-    UIButton.ButtonSize.Small,
-    UIButton.ButtonSize.Default
-    @default UIButton.ButtonSize.Medium
-    */
-    buttonSize?: string,
-    /** use it for additional data
-    @default ''
-    */
-    count?: string,
-    /** use it for additional data
-    @default ''
-    */
-    data?: string,
-    /** use it for default left icon, ignore it if use icon prop
-    @default false
-    */
-    hasIcon?: boolean,
-    /** use it for default right icon, ignore it if use iconR prop
-    @default false
-    */
-    hasIconR?: boolean,
+type Props = ButtonProps & {
     /** external url, starting with http...
     @default null
     */
     href?: ?string,
-    /** uri to left icon
-    @default null
-    */
-    icon?: ?string,
-    /** uri to right icon
-    @default null
-    */
-    iconR?: ?string,
-    /** specify in addition to showIndicator props, one of:
-        UIButton.Indicator.Spin,
-        UIButton.Indicator.Round,
-        UIButton.ButtonStyle.Sandglass,
-        UIButton.ButtonStyle.Pulse
-        @default null
-     */
-    indicatorAnimation?: string,
-    /** button container style
-    @default null
-    */
-    style?: StylePropType,
-    /** text align, one of:
-    UIButton.TextAlign.Center,
-    UIButton.TextAlign.Left,
-    UIButton.TextAlign.Right,
-    @default UIButton.TextAlign.Center
-    */
-    textAlign?: string,
-    /** button title style
-    @default null
-    */
-    textStyle?: StylePropType,
-    /** Visible button title
-    @default ''
-    */
-    title?: string,
-    /** @ignore */
-    theme?: string,
-
-    /** set true if button disabled
-    @default false
-    */
-    disabled?: boolean,
-    /** set true for indicating wait state
-    @default false
-    */
-    showIndicator?: boolean,
-    /** Your action here */
-    onPress?: () => void,
-    /** Your action here */
-    onMouseEnter?: () => void,
-    /** Your action here */
-    onMouseLeave?: () => void,
 };
 
 type State = {};
@@ -122,21 +48,6 @@ export default class UILink extends UIComponent<Props, State> {
 }
 
 UILink.defaultProps = {
-    buttonSize: UIButton.ButtonSize.Medium,
-    count: '',
-    data: '',
-    hasIcon: false,
-    hasIconR: false,
+    ...UIButton.defaultProps,
     href: null,
-    icon: null,
-    theme: UIColor.Theme.Light,
-    title: '',
-    iconR: null,
-    style: null,
-    textAlign: UIButton.TextAlign.Center,
-    textStyle: null,
-    indicatorAnimation: '',
-
-    disabled: false,
-    showIndicator: false,
 };
