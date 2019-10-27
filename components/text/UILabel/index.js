69d07e6dcef835718f1166d8d4744f2d9079f7d1
khvmkh <mokhkhvoya@gmail.com>
2019-10-27 19:44:06 +0300

    UI label update

--- components/text/UILabel/index.js
+++ components/text/UILabel/index.js
@@ -156,13 +156,13 @@ export default class UILabel extends UIComponent<Props, State> {
     renderText(textStyle: TextStyleProp[]): React$Node {
         const {
             useDefaultSpace,
-            role, text,
+            role, text, testID,
             ...props
         } = this.props;
         return (
             <Text
                 {...props}
-                testID={`label_text_${text}`}
+                testID={testID || `label_text_${text}`}
                 style={textStyle}
             >
                 {this.getText()}
