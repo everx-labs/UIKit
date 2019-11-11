1c37c91de522477e7ba04a76c66c0a4d6158b4f9
Elizaveta Makarova <vetka.mak@gmail.com>
2019-11-11 12:58:47 +0700

    set balance token before balance number

--- components/views/UIBalanceView/index.js
+++ components/views/UIBalanceView/index.js
@@ -16,6 +16,11 @@ type ColoredDigit = {
     primary?: boolean,
 };
 
+type BalanceWithSign = {
+    balance: string,
+    sign: string,
+};
+
 type Props = {
     animated: boolean,
     balance: string,
@@ -319,16 +324,35 @@ export default class UIBalanceView extends UIComponent<Props, State> {
         });
     }
 
+    formBalanceWithSign(balance: string): BalanceWithSign {
+        if (balance.includes('− ') || balance.includes('- ')) {
+            return {
+                balance: balance.replace('− ', '').replace('- ', ''),
+                sign: '−',
+            };
+        } else if (balance.includes('+ ') || balance.includes('+ ')) {
+            return {
+                balance: balance.replace('+ ', '').replace('+ ', ''),
+                sign: '+',
+            };
+        }
+        return {
+            balance,
+            sign: '',
+        };
+    }
+
     // Render
     renderBalance() {
         const balance = this.getBalance();
+        const balanceWithSign: BalanceWithSign = this.formBalanceWithSign(balance);
         const separator = this.getSeparator();
-        const stringParts = balance.split(separator);
-        const integer = stringParts[0];
+        const stringParts = balanceWithSign.balance.split(separator);
+        const integer = `${balanceWithSign.sign ? `${balanceWithSign.sign} ` : ''}${this.getTokenSymbol()} ${stringParts[0]}`;
         const fractional = stringParts.length > 1
             ? (
                 <Text style={this.props.fractionalTextStyle}>
-                    {`${separator}${stringParts[1]} ${this.getTokenSymbol()}`}
+                    {`${separator}${stringParts[1]}`}
                 </Text>
             )
             : null;
