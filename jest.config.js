dab0ea0c2e11e8d27d8185b50a4695b7d7755a96
Panteleev Philipp <PanteleevPhil@gmail.com>
2020-01-22 17:31:57 +0400

    Comment remove

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
 module.exports = {
     setupFiles: [
       '<rootDir>/jest-setup.js'
