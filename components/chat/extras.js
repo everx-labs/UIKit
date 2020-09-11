ba045fdb1a7b421649df36bd27d481c05964d37f
Jose <8920394+jcma86@users.noreply.github.com>
2020-09-11 10:56:45 +0300

    Types for stickers (#728)
    * Added types for stickers
    
    * Type updated

--- components/chat/extras.js
+++ components/chat/extras.js
@@ -115,3 +115,17 @@ export type UIChatCellInfo = {
     additionalInfo: ChatAdditionalInfo,
     key: string,
 };
+
+export type UISticker = {
+    name: string,
+    keywords?: ?string[],
+    url: string,
+};
+
+export type UIStickerPackage = {
+    id: string,
+    date: number,
+    description: string,
+    name: string,
+    stickers: UISticker[],
+};
