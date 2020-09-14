e3d7f5bd8cdc1e3c3f9c8214e74e7485d51a80fe
Jose Cruz Marquez Aguilar <jose@tonlabs.io>
2020-09-15 01:14:46 +0300

    Updated UIImage to use FastImage Updated UIImageView to use UIImage component Updated UIChatStickerCell to use UIImage component

--- components/images/UIImage/index.js
+++ components/images/UIImage/index.js
@@ -1,11 +1,15 @@
 // @flow
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Platform } from 'react-native';
 import type { ImageSource } from 'react-native/Libraries/Image/ImageSource';
 
 import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
 
 import UIColor from '../../../helpers/UIColor';
+
+const FastImage = Platform.OS !== 'web' ? require('react-native-fast-image').default : null;
+
+const ImageComponent: any = Platform.OS === 'web' ? Image : FastImage;
 
 type Props = {
     color?: string,
@@ -16,7 +20,7 @@ type Props = {
 const UIImage = ({ color, source, style }: Props) => {
     const colorStyle = color ? UIColor.getTintColorStyle(color) : null;
     return (
-        <Image
+        <ImageComponent
             source={source}
             style={[colorStyle, style]}
         />
