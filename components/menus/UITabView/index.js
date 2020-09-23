65b311d9c7676163d899bd87b1f573958ee44959
Alexander <aldelank@gmail.com>
2020-09-23 12:33:45 +0300

    rewrite UITabView to func

--- components/menus/UITabView/index.js
+++ components/menus/UITabView/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { View, StyleSheet, TouchableWithoutFeedback, Animated } from 'react-native';
 import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
 import type AnimatedValue from 'react-native/Libraries/Animated/src/nodes/AnimatedValue';
@@ -8,7 +8,6 @@ import type AnimatedMultiplication from 'react-native/Libraries/Animated/src/nod
 import UIColor from '../../../helpers/UIColor';
 import UIConstant from '../../../helpers/UIConstant';
 import UIStyle from '../../../helpers/UIStyle';
-import UIComponent from '../../UIComponent';
 import UITextButton from '../../buttons/UITextButton';
 
 export type TabViewPage = {
@@ -25,10 +24,6 @@ export type TabViewProps = {
     initialIndex?: number,
 };
 
-type State = {
-    integerIndex: number,
-};
-
 const styles = StyleSheet.create({
     bottomLine: {
         height: 2,
@@ -36,123 +31,114 @@ const styles = StyleSheet.create({
     },
 });
 
-export default class UITabView extends UIComponent<TabViewProps, State> {
-    static defaultProps: TabViewProps = {
-        pages: [],
-        width: 0,
-        initialIndex: 0,
-    };
-
-    static testIDs = {
-        tabTitle: (title: string) => `tabTitle_${title}`,
-    };
-
-    animatedIndex: AnimatedValue = new Animated.Value(this.props.initialIndex || 0);
-
-    state = {
-        integerIndex: this.props.initialIndex || 0,
-    };
+const UITabView = ({
+    pages,
+    width,
+    indicatorWidth,
+    style,
+    pageStyle,
+    initialIndex,
+}: TabViewProps) => {
+    // const animatedIndex = useRef(new Animated.Value(initialIndex)).current;
+    // const [animatedIndex] = useState<AnimatedValue>(new Animated.Value(initialIndex));
+    const [integerIndex, setIntegerIndex] = useState<number>(initialIndex);
+    const animatedIndex = 0;
+    // const integerIndex = 0;
 
     // Events
-    onPressTab = (index: number) => {
-        Animated.timing(this.animatedIndex, {
+    const onPressTab = (index: number) => {
+        Animated.timing(animatedIndex, {
             toValue: index,
             useNativeDriver: true,
             duration: UIConstant.animationDuration(),
         }).start();
-        this.setStateSafely({ integerIndex: index });
+        // setIntegerIndex(index);
     };
 
     // Getters
-    getMarginLeft(width: number): AnimatedMultiplication {
-        return Animated.multiply(this.animatedIndex, new Animated.Value(-width));
-    }
+    const getMarginLeft = (widthParam: number): AnimatedMultiplication => {
+        return Animated.multiply(animatedIndex, new Animated.Value(-widthParam));
+    };
 
     // Render
-    renderTapBar() {
-        return (
-            <Animated.View style={UIStyle.common.flexRow()}>
-                {this.props.pages.map(({ title }: TabViewPage, index: number) => {
-                    const textStyle = index === this.state.integerIndex
-                        ? UIStyle.text.actionBodyBold()
-                        : UIStyle.text.secondaryBodyBold();
+    const tapBar = (
+        <Animated.View style={UIStyle.flex.row()}>
+            {pages.map(({ title }: TabViewPage, index: number) => {
+                const textStyle = index === integerIndex
+                    ? UIStyle.text.actionBodyBold()
+                    : UIStyle.text.secondaryBodyBold();
+                return (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <TouchableWithoutFeedback
+                        onPress={() => onPressTab(index)}
+                        key={`tab-view-label-${title}`}
+                    >
+                        <View style={[UIStyle.flex.x1(), UIStyle.flex.alignCenter()]}>
+                            <UITextButton
+                                title={title}
+                                textStyle={textStyle}
+                                // onPress={() => onPressTab(index)}
+                                testID={UITabView.testIDs.tabTitle(title)}
+                            />
+                        </View>
+                    </TouchableWithoutFeedback>
+                );
+            })}
+        </Animated.View>
+    );
+
+    const marginLeft = Animated.divide(
+        getMarginLeft(indicatorWidth || width),
+        new Animated.Value(-pages.length),
+    );
+
+    const indicatorLine = (pages.length && width) && (
+        <View style={[UIStyle.flex.x1(), UIStyle.common.overflowHidden()]}>
+            <Animated.View
+                style={[
+                    styles.bottomLine,
+                    {
+                        width: (indicatorWidth || width) / pages.length,
+                        marginLeft,
+                    },
+                ]}
+            />
+        </View>
+    );
+
+    const pagesComponent = (
+        <View style={[UIStyle.common.overflowHidden(), pageStyle]}>
+            <Animated.View
+                style={[UIStyle.flex.row(), { marginLeft: getMarginLeft(width) }]}
+            >
+                {pages.map(({ title, component }: TabViewPage) => {
                     return (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <TouchableWithoutFeedback
-                            onPress={() => this.onPressTab(index)}
-                            key={`tab-view-label-${title}`}
-                        >
-                            <View style={[UIStyle.common.flex(), UIStyle.common.alignCenter()]}>
-                                <UITextButton
-                                    title={title}
-                                    textStyle={textStyle}
-                                    onPress={() => this.onPressTab(index)}
-                                    testID={UITabView.testIDs.tabTitle(title)}
-                                />
-                            </View>
-                        </TouchableWithoutFeedback>
+                        <View style={{ width }} key={`tab-view-page-${title}`}>
+                            {component}
+                        </View>
                     );
                 })}
             </Animated.View>
-        );
-    }
+        </View>
+    );
 
-    renderIndicatorLine() {
-        const { width, indicatorWidth, pages } = this.props;
-        if (!pages.length || !width) {
-            return null;
-        }
+    return !!width && (
+        <View style={[UIStyle.flex.x1(), style]}>
+            {tapBar}
+            {indicatorLine}
+            {pagesComponent}
+        </View>
+    );
+};
 
-        const marginLeft = Animated.divide(
-            this.getMarginLeft(indicatorWidth || width),
-            new Animated.Value(-pages.length),
-        );
+UITabView.defaultProps = {
+    pages: [],
+    width: 0,
+    initialIndex: 0,
+};
 
-        return (
-            <View style={[UIStyle.common.flex(), UIStyle.common.overflowHidden()]}>
-                <Animated.View
-                    style={[
-                        styles.bottomLine,
-                        {
-                            width: (indicatorWidth || width) / pages.length,
-                            marginLeft,
-                        },
-                    ]}
-                />
-            </View>
-        );
-    }
+UITabView.testIDs = {
+    tabTitle: (title: string) => `tabTitle_${title}`,
+};
 
-    renderPages() {
-        const { width, pages, pageStyle } = this.props;
-        return (
-            <View style={[UIStyle.common.overflowHidden(), pageStyle]}>
-                <Animated.View
-                    style={[UIStyle.common.flexRow(), { marginLeft: this.getMarginLeft(width) }]}
-                >
-                    {pages.map(({ title, component }: TabViewPage) => {
-                        return (
-                            <View style={{ width }} key={`tab-view-page-${title}`}>
-                                {component}
-                            </View>
-                        );
-                    })}
-                </Animated.View>
-            </View>
-        );
-    }
-
-    render() {
-        if (!this.props.width) {
-            return null;
-        }
-
-        return (
-            <View style={[UIStyle.common.flex(), this.props.style]}>
-                {this.renderTapBar()}
-                {this.renderIndicatorLine()}
-                {this.renderPages()}
-            </View>
-        );
-    }
-}
+export default UITabView;
