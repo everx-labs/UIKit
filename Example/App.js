05e3df3e0f5512cbb4751aa6834d8c0e664ef871
Aleksei Saveliev <savelichalex93@gmail.com>
2020-10-09 11:36:13 +0300

    Add notifications examples

--- Example/App.js
+++ Example/App.js
@@ -72,6 +72,10 @@ import {
     UISlider,
     UIStepBar,
     UITabView,
+    UINotice,
+    UINotificationBadge,
+    UIToastMessage,
+    UITooltip,
 } from "../UIKit";
 import UIAssets from "../assets/UIAssets";
 
@@ -114,6 +118,11 @@ const Main = ({ navigation }) => (
             onPress={() => navigation.navigate("menus")}
             buttonStyle={UIButton.ButtonStyle.Link}
             title="Menus"
+        />
+        <UIButton
+            onPress={() => navigation.navigate("notifications")}
+            buttonStyle={UIButton.ButtonStyle.Link}
+            title="Notifications"
         />
     </SafeAreaView>
 );
@@ -1667,6 +1676,107 @@ const Menus = () => {
     );
 };
 
+const Notifications = () => (
+    <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+        <View
+            style={{
+                width: "96%",
+                paddingLeft: 40,
+                paddingBottom: 10,
+                marginHorizontal: "2%",
+                marginTop: 20,
+                borderBottomWidth: 1,
+                borderBottomColor: "rgba(0,0,0,.1)",
+            }}
+        >
+            <Text>UINotice</Text>
+        </View>
+        <View style={{ maxWidth: 300, paddingVertical: 20 }}>
+            <UITextButton
+                title="Show default notice with message only"
+                onPress={() =>
+                    UINotice.showMessage(
+                        "System is going down at midnight tonight. We’ll notify you when it’s back up."
+                    )
+                }
+            />
+        </View>
+        <View
+            style={{
+                width: "96%",
+                paddingLeft: 40,
+                paddingBottom: 10,
+                marginHorizontal: "2%",
+                marginTop: 50,
+                borderBottomWidth: 1,
+                borderBottomColor: "rgba(0,0,0,.1)",
+            }}
+        >
+            <Text>UINotificationBadge</Text>
+        </View>
+        <View style={{ maxWidth: 300, paddingVertical: 20 }}>
+            <UINotificationBadge value={100} />
+        </View>
+        <View
+            style={{
+                width: "96%",
+                paddingLeft: 40,
+                paddingBottom: 10,
+                marginHorizontal: "2%",
+                marginTop: 50,
+                borderBottomWidth: 1,
+                borderBottomColor: "rgba(0,0,0,.1)",
+            }}
+        >
+            <Text>UIToastMessage</Text>
+        </View>
+        <View style={{ maxWidth: 300, paddingVertical: 20 }}>
+            <UITextButton
+                title="Show default notice with message only"
+                onPress={() =>
+                    UIToastMessage.showMessage(
+                        "System is going down at midnight tonight."
+                    )
+                }
+            />
+        </View>
+        <View
+            style={{
+                width: "96%",
+                paddingLeft: 40,
+                paddingBottom: 10,
+                marginHorizontal: "2%",
+                marginTop: 50,
+                borderBottomWidth: 1,
+                borderBottomColor: "rgba(0,0,0,.1)",
+            }}
+        >
+            <Text>UITooltip</Text>
+        </View>
+        <View style={{ maxWidth: 300, paddingVertical: 20 }}>
+            <UITooltip message="Message one">
+                <Text style={{ fontSize: 16 }}> Trigger 1</Text>
+            </UITooltip>
+            <UITooltip message="Message two with more text for two lines to see second option of layout.">
+                <Text style={{ fontSize: 16 }}> Trigger 2</Text>
+            </UITooltip>
+            <UITooltip message="Message three is huge, with five lines of text wich contains more usefull information for all users and many-many bla-bla-bla to see maximum height of tooltip. You can add here some instructions.">
+                <Text style={{ fontSize: 16 }}> Trigger 3</Text>
+            </UITooltip>
+            <UITextButton
+                title="Show onMouse tooltip"
+                onPress={() =>
+                    UITooltip.showOnMouseForWeb("Message of onMouse tooltip")
+                }
+            />
+            <UITextButton
+                title="Hide onMouse tooltip"
+                onPress={() => UITooltip.hideOnMouseForWeb()}
+            />
+        </View>
+    </ScrollView>
+);
+
 const App: () => React$Node = () => {
     const navRef = React.useRef();
     useReduxDevToolsExtension(navRef);
@@ -1693,6 +1803,10 @@ const App: () => React$Node = () => {
                     <SurfSplit.Screen name="images" component={Images} />
                     <SurfSplit.Screen name="layouts" component={Layouts} />
                     <SurfSplit.Screen name="menus" component={Menus} />
+                    <SurfSplit.Screen
+                        name="notifications"
+                        component={Notifications}
+                    />
                 </SurfSplit.Navigator>
             </NavigationContainer>
             <UILayoutManager />
@@ -1714,6 +1828,9 @@ const App: () => React$Node = () => {
                     </>
                 }
             />
+            <View style={StyleSheet.absoluteFill} pointerEvents="box-none">
+                <UINotice />
+            </View>
         </>
     );
 
