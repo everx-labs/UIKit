3769acee1123105588a9bd6e7c754df7d93d8e66
Aleksei Saveliev <savelichalex93@gmail.com>
2020-10-15 16:45:41 +0300

    Put missing flow annotation

--- packages/navigation/src/index.js
+++ packages/navigation/src/index.js
@@ -1,3 +1,4 @@
+// @flow
 export { default as UIController } from './UIController';
 export { default as UIModalController } from './UIModalController';
 export { default as UIModalNavigationBar } from './UIModalController/UIModalNavigationBar';
