7a04d3d2305ad1f271eadcea8f7b22b5006500a7
Alexey Savelev <savelichalex93@gmail.com>
2020-11-11 15:39:05 +0300

    Move system message to new package (#814)

--- packages/chats/src/BubbleSystem.tsx
+++ packages/chats/src/BubbleSystem.tsx
@@ -1,1 +1,28 @@
-export const BubbleSystem = () => null;
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { UIStyle } from "@uikit/core";
+import { UILabel } from "@uikit/components";
+
+import type { SystemMessage } from "./types";
+
+export function BubbleSystem(props: SystemMessage) {
+    return (
+        <View style={styles.container}>
+            <UILabel
+                role={UILabel.Role.TinyRegular}
+                text={props.text}
+                style={UIStyle.color.textTertiary()}
+                numberOfLines={1}
+                ellipsizeMode="middle"
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignSelf: "center",
+        justifyContent: "center",
+    },
+});
