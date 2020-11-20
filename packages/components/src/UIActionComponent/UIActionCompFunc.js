b8e8533fd809b44766406186774109b5910a272b
Alexander <31403308+aldelank@users.noreply.github.com>
2020-11-20 16:02:50 +0300

    Ton live testhost (#793)
    * add new features and bug fixes
    
    * add validators data for block details
    
    * fix for PR
    
    * add new localized keys
    
    * add func components
    
    * fixes for review
    
    * fix pressing for touchable
    
    * update UIKit
    
    * fix for review

---
+++ packages/components/src/UIActionComponent/UIActionCompFunc.js
@@ -0,0 +1,92 @@
+// @flow
+import React, { useRef, useState } from 'react';
+import { TouchableWithoutFeedback, View } from 'react-native';
+
+export type ActionProps = {
+    testID?: string,
+    disabled?: boolean,
+    showIndicator?: boolean,
+    onPress?: () => void,
+    onMouseEnter?: () => void,
+    onMouseLeave?: () => void,
+    children?: React$Element<any>,
+};
+
+type Props = any & ActionProps
+
+const UIActionComponent = ({
+                               testID = '',
+                               disabled = false,
+                               showIndicator = false,
+                               onPress = null,
+                               children,
+                               onMouseEnter,
+                               onMouseLeave,
+                           }: Props) => {
+    const [isTapped, setTapped] = useState<boolean>(false);
+    const [isHover, setHover] = useState<boolean>(false);
+    const childrenRef = useRef(null);
+
+    // Events
+    const onPressIn = () => {
+        setTapped(true);
+        childrenRef.current?.onTappedIn && childrenRef.current.onTappedIn();
+    };
+
+    const onPressOut = () => {
+        setTapped(false);
+        childrenRef.current?.onTappedOut && childrenRef.current.onTappedOut();
+    };
+
+    const onPressHandler = () => {
+        childrenRef.current?.onPressed && childrenRef.current.onPressed();
+        if (onPress) {
+            onPress();
+        }
+    };
+
+    const onMouseEnterHandler = () => {
+        setHover(true);
+        childrenRef.current?.onEnter && childrenRef.current.onEnter();
+        if (onMouseEnter) {
+            onMouseEnter();
+        }
+    };
+
+    const onMouseLeaveHandler = () => {
+        setHover(false);
+        childrenRef.current?.onLeave && childrenRef.current.onLeave();
+        if (onMouseLeave) {
+            onMouseLeave();
+        }
+    };
+
+    const testIDProp = testID ? { testID } : null;
+
+    return (
+        <TouchableWithoutFeedback
+            {...testIDProp}
+            disabled={disabled || showIndicator || !onPress}
+            onPress={onPressHandler}
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
+        >
+            <View
+                onMouseEnter={onMouseEnterHandler}
+                onMouseLeave={onMouseLeaveHandler}
+            >
+                {React.cloneElement(children, {
+                    ref: childrenRef,
+                    isTapped,
+                    isHover,
+                    style: [{
+                        cursor: 'pointer',
+                        touchAction: 'manipulation',
+                    }, children.props.style],
+                })}
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+
+export default UIActionComponent;
--- packages/components/src/UIToggle/UIToggleFunc.js
+++ packages/components/src/UIToggle/UIToggleFunc.js
@@ -1,4 +1,3 @@
-// this component can't be used now, because of some error in react inside UIKit
 // @flow
 import React, { useCallback } from 'react';
 import { View, Image, TouchableWithoutFeedback } from 'react-native';
@@ -10,22 +9,22 @@ import { UIAssets } from '@tonlabs/uikit.assets';
 type Props = {
     iconActive: ?ImageSource,
     iconInactive: ?ImageSource,
-    containerStyle: ViewStyleProp,
+    containerStyle?: ViewStyleProp,
     active: boolean,
-    colored: boolean,
+    colored?: boolean,
     onPress: boolean => void,
     testID: ?string,
 };
 
 const UIToggle = ({
-    active = false,
-    onPress = () => {},
-    colored = false,
-    iconActive = null,
-    iconInactive = null,
-    containerStyle = {},
-    testID = null,
-}: Props) => {
+                      active = false,
+                      onPress = () => {},
+                      colored = false,
+                      iconActive = null,
+                      iconInactive = null,
+                      containerStyle = {},
+                      testID = null,
+                  }: Props) => {
     const onPresHandler = useCallback(() => {
         onPress(!active);
     }, [active]);
@@ -48,18 +47,14 @@ const UIToggle = ({
             {...testIDProp}
             onPress={onPresHandler}
         >
-            <View
-                style={[containerStyle, {
-                    cursor: 'pointer',
-                    touchAction: 'manipulation',
-                }]}
+            <View style={[containerStyle, {
+                cursor: 'pointer',
+                touchAction: 'manipulation',
+            }]}
             >
-                <Image
-                    source={source}
-                />
+                <Image source={source} />
             </View>
         </TouchableWithoutFeedback>
     );
 };
-
 export default UIToggle;
