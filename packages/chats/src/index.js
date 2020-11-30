765918eac4103a989f32a047e2e0640b3d3b76f0
Aleksei Saveliev <savelichalex93@gmail.com>
2020-11-30 16:41:09 +0300

    Export UICustomKeyboard and it's utils

--- packages/chats/src/index.js
+++ packages/chats/src/index.js
@@ -1,4 +1,8 @@
 export * from './UIChatList';
 export * from './UIChatInput';
+export {
+    UICustomKeyboardUtils,
+    UICustomKeyboard,
+} from './UIChatInput/UICustomKeyboard';
 
 export * from './types';
