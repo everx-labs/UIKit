19d9d2db3dfb1bac9de844ed91e235c8e0cbc5a3
Elizaveta Makarova <vetka.mak@gmail.com>
2021-02-11 19:01:19 +0700

    upd kb back color

--- packages/stickers/src/useStickers.tsx
+++ packages/stickers/src/useStickers.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import type { UICustomKeyboardItem } from '@tonlabs/uikit.keyboard';
+import { useTheme } from '@tonlabs/uikit.hydrogen';
 
 import { StickersButton } from './StickersButton';
 import { StickerPickerKeyboardName, StickersList } from './StickersKeyboard';
@@ -9,6 +10,7 @@ export function useStickers(
     stickers: UIStickerPackage[],
     onItemSelected: OnPickSticker,
 ): UICustomKeyboardItem {
+    const theme = useTheme();
     const stickersKeyboard = React.useMemo(() => {
         return {
             button: StickersButton,
@@ -16,10 +18,11 @@ export function useStickers(
             component: StickersList,
             props: {
                 stickers,
+                theme,
             },
             onItemSelected,
         };
-    }, [stickers, onItemSelected]);
+    }, [stickers, onItemSelected, theme]);
 
     return stickersKeyboard;
 }
