2e7d459154dae9285f8f38fdc60781cf9ac804cc
Dmitry Romanov <romanov.ds35@gmail.com>
2021-02-18 14:31:51 +0300

    add props for UIBanner

--- packages/components/src/UIBanner/index.js
+++ packages/components/src/UIBanner/index.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import StylePropType from 'react-style-proptype';
 
 import { UIStyle } from '@tonlabs/uikit.core';
 import {
@@ -14,29 +15,42 @@ import UIComponent from '../UIComponent';
 
 type Props = {
     text: ?string,
+    backgroundColor: ?$Values<typeof UIBackgroundViewColors>,
+    textColor: ?$Values<typeof UILabelColors>,
+    style: StylePropType,
 };
 
 type State = {};
 
 export default class UIBanner extends UIComponent<Props, State> {
+    // eslint-disable-next-line class-methods-use-this
+    getBackgroundColor() {
+        return this.props.backgroundColor || UIBackgroundViewColors.BackgroundNegative;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    getTextColor() {
+        return this.props.textColor || UILabelColors.TextPrimaryInverted;
+    }
+
     render() {
+        const style = [
+            UIStyle.margin.topSmall(),
+            UIStyle.margin.bottomSmall(),
+            UIStyle.padding.verticalNormal(),
+            UIStyle.padding.horizontalNormal(),
+            UIStyle.border.radiusDefault(),
+            { display: this.props.text ? 'flex' : 'none' },
+        ];
+
         return (
             <UIBackgroundView
-                color={UIBackgroundViewColors.BackgroundNegative}
-                style={[
-                    UIStyle.margin.topSmall(),
-                    UIStyle.margin.bottomSmall(),
-                    UIStyle.margin.leftDefault(),
-                    UIStyle.margin.rightDefault(),
-                    UIStyle.padding.verticalNormal(),
-                    UIStyle.padding.horizontalNormal(),
-                    UIStyle.border.radiusDefault(),
-                    { display: this.props.text ? 'flex' : 'none' },
-                ]}
+                color={this.getBackgroundColor()}
+                style={[style, this.props.style]}
             >
                 <UILabel
                     role={UILabelRoles.ActionCallout}
-                    color={UILabelColors.TextPrimaryInverted}
+                    color={this.getTextColor()}
                 >
                     {this.props.text || ''}
                 </UILabel>
