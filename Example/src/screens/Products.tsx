4a2ac37f277a61f0f90bab9ecd0a9b8340728dcd
Elizaveta Makarova <vetka.mak@gmail.com>
2021-03-31 14:55:06 +0700

    move promo notice to products section & add toggle (#1001)

--- Example/src/screens/Products.tsx
+++ Example/src/screens/Products.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { View } from 'react-native';
 
+import { UIDetailsToggle } from '@tonlabs/uikit.components';
+import { UIPromoNotice } from '@tonlabs/uikit.flask';
 import { UIBottomBar } from '@tonlabs/uikit.navigation_legacy';
 import {
     UIFeedback,
@@ -11,47 +14,71 @@ import {
 import { ExampleSection } from '../components/ExampleSection';
 import { ExampleScreen } from '../components/ExampleScreen';
 
-export const Products = () => (
-    <ExampleScreen>
-        <ExampleSection title="UIBottomBar">
-            <View style={{ maxWidth: 500, height: 180, paddingVertical: 20 }}>
-                <UIBottomBar
-                    testID="uiBottomBar_info"
-                    leftText="Feedback"
-                    companyName="Wallet solutions OÜ"
-                    address="Jõe 2"
-                    postalCode="10151"
-                    location="Tallinn, Estonia"
-                    email="os@ton.space"
-                    phoneNumber="+372 7124030"
-                    copyRight="2018-2019 © TON Labs"
-                />
-            </View>
-        </ExampleSection>
-        <ExampleSection title="UIFeedback">
-            <View style={{ maxWidth: 500, paddingVertical: 20 }}>
-                <UIFeedback testID="uiFeedback_default" />
-            </View>
-        </ExampleSection>
-        <ExampleSection title="UIPushFeedback">
-            <View style={{ maxWidth: 500, paddingVertical: 20 }}>
-                <UIPushFeedback testID="uiPushFeedback_message" onPress={() => undefined} />
-            </View>
-        </ExampleSection>
-        <ExampleSection title="UIStubPage">
-            <View style={{ maxWidth: 500, paddingVertical: 20 }}>
-                <UIStubPage testID="uiStubPage_default" title="labs." needBottomIcon={false} />
-            </View>
-        </ExampleSection>
-        <ExampleSection title="UITokenCell">
-            <View style={{ width: 300, paddingVertical: 20 }}>
-                <UITokenCell
-                    testID="uiTokenCell_Gram"
-                    title="GRAM"
-                    details="balance"
-                    balance="100.0000"
-                />
-            </View>
-        </ExampleSection>
-    </ExampleScreen>
-);
+export const Products = () => {
+    const [displayPromoNotice, setDisplayPromoNotice] = useState(false);
+    return (
+        <ExampleScreen>
+            <ExampleSection title="UIBottomBar">
+                <View style={{ maxWidth: 500, height: 180, paddingVertical: 20 }}>
+                    <UIBottomBar
+                        testID="uiBottomBar_info"
+                        leftText="Feedback"
+                        companyName="Wallet solutions OÜ"
+                        address="Jõe 2"
+                        postalCode="10151"
+                        location="Tallinn, Estonia"
+                        email="os@ton.space"
+                        phoneNumber="+372 7124030"
+                        copyRight="2018-2019 © TON Labs"
+                    />
+                </View>
+            </ExampleSection>
+            <ExampleSection title="UIFeedback">
+                <View style={{ maxWidth: 500, paddingVertical: 20 }}>
+                    <UIFeedback testID="uiFeedback_default" />
+                </View>
+            </ExampleSection>
+            <ExampleSection title="UIPromoNotice">
+                <View style={{ maxWidth: 300, paddingVertical: 20 }}>
+                    <UIDetailsToggle
+                        testID="uiPromoNotice_display_toggle"
+                        details="Display UIPromoNotice"
+                        active={displayPromoNotice}
+                        onPress={() => {
+                            setDisplayPromoNotice(!displayPromoNotice)
+                        }}
+                        switcherPosition={UIDetailsToggle.Position.Left}
+                    />
+                </View>
+                {
+                    displayPromoNotice && (
+                        <UIPromoNotice
+                            appStoreUrl="https://www.apple.com/app-store/"
+                            googlePlayUrl="https://play.google.com/store"
+                        />
+                    )
+                }
+            </ExampleSection>
+            <ExampleSection title="UIPushFeedback">
+                <View style={{ maxWidth: 500, paddingVertical: 20 }}>
+                    <UIPushFeedback testID="uiPushFeedback_message" onPress={() => undefined} />
+                </View>
+            </ExampleSection>
+            <ExampleSection title="UIStubPage">
+                <View style={{ maxWidth: 500, paddingVertical: 20 }}>
+                    <UIStubPage testID="uiStubPage_default" title="labs." needBottomIcon={false} />
+                </View>
+            </ExampleSection>
+            <ExampleSection title="UITokenCell">
+                <View style={{ width: 300, paddingVertical: 20 }}>
+                    <UITokenCell
+                        testID="uiTokenCell_Gram"
+                        title="GRAM"
+                        details="balance"
+                        balance="100.0000"
+                    />
+                </View>
+            </ExampleSection>
+        </ExampleScreen>
+    );
+};
