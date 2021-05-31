8e12a65e43ddfe84cbb539f268a47e35314e3464
Aleksei Saveliev <savelichalex93@gmail.com>
2021-05-31 13:01:25 +0300

    Update dependencies declarations

--- packages/navigation/src/SplitNavigator/createSplitNavigator.tsx
+++ packages/navigation/src/SplitNavigator/createSplitNavigator.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import {
-    useWindowDimensions as useWindowDimensionsNative,
+    useWindowDimensions,
     StyleSheet,
     View,
     ViewStyle,
-    Dimensions,
     Platform,
-    ScaledSize,
     StyleProp,
 } from 'react-native';
 import {
@@ -36,37 +34,6 @@ import type { SplitNavigationState, SplitRouterOptions } from './SplitRouter';
 
 const getIsSplitted = ({ width }: { width: number }, mainWidth: number) =>
     width > mainWidth;
-
-const useWindowDimensions =
-    useWindowDimensionsNative ||
-    function useWindowDimensionsFallback() {
-        const [dimensions, setDimensions] = React.useState(() =>
-            Dimensions.get('window'),
-        );
-
-        React.useEffect(() => {
-            function handleChange({ window }: { window: ScaledSize }) {
-                if (
-                    dimensions.width !== window.width ||
-                    dimensions.height !== window.height ||
-                    dimensions.scale !== window.scale ||
-                    dimensions.fontScale !== window.fontScale
-                ) {
-                    setDimensions(window);
-                }
-            }
-            Dimensions.addEventListener('change', handleChange);
-            // We might have missed an update between calling `get` in render and
-            // `addEventListener` in this handler, so we set it here. If there was
-            // no change, React will filter out this update as a no-op.
-            handleChange({ window: Dimensions.get('window') });
-            return () => {
-                Dimensions.removeEventListener('change', handleChange);
-            };
-        }, [dimensions]);
-
-        return dimensions;
-    };
 
 function SceneContent({
     isFocused,
