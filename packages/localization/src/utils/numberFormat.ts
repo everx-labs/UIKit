56eebf589dd64962449525b371ceb4fba0b6d8e1
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-06-22 18:51:42 +0300

    Add number formater.

--- packages/localization/src/utils/numberFormat.ts
+++ packages/localization/src/utils/numberFormat.ts
@@ -56,9 +56,13 @@ const formatBigNumber = (
 };
 
 export const numberFormat = (
-    value: number | BigNumber,
+    value: number | BigNumber | null,
     settings?: NumberFormatSettings,
 ): string => {
+    if (value === null) {
+        return ''
+    }
+
     let bigNumberValue: BigNumber;
     if (!BigNumber.isBigNumber(value)) {
         try {
@@ -76,101 +80,3 @@ export const numberFormat = (
     }
     return formatBigNumber(bigNumberValue, settings);
 };
-
-/**
- * @worklet
- */
-const convertPowerOfThousandToPowerOfTenForDecimalSuffixesOnUI = (
-    powerOfThousand: number,
-): PowerOfTenForDecimalSuffixes => {
-    'worklet';
-
-    const powerOfTen: number = powerOfThousand * 3;
-    return powerOfTen.toFixed() as PowerOfTenForDecimalSuffixes;
-};
-
-/**
- * @worklet
- */
-const getSuffixOnUI = (powerOfThousand: number): string => {
-    'worklet';
-
-    const powerOfTenForDecimalSuffixes: PowerOfTenForDecimalSuffixes = convertPowerOfThousandToPowerOfTenForDecimalSuffixesOnUI(
-        powerOfThousand,
-    );
-    return uiLocalized.DecimalSuffixes[powerOfTenForDecimalSuffixes];
-};
-
-/**
- * @worklet
- */
-export const formatNumberOnUI = (
-    value: number,
-    { digitsAfterDecimalPoint: DEFAULT_DIGITS_AFTER_DECIMAL_POINT }: NumberFormatSettings = {},
-): string => {
-    'worklet';
-
-    const digitsAfterDecimalPoint: number =
-        settings && settings.digitsAfterDecimalPoint
-            ? settings.digitsAfterDecimalPoint
-            : DEFAULT_DIGITS_AFTER_DECIMAL_POINT;
-    
-    const powerOfThousand: number = Math.floor(Math.log10(Math.abs(value)) / 3);
-
-    if (powerOfThousand <= 0) {
-        return value.toFixed(digitsAfterDecimalPoint);
-    }
-
-    const suffix: string = getSuffixOnUI(powerOfThousand);
-
-    const scale: number = 1000 ** powerOfThousand;
-    const scaledValue: number = value / scale;
-    return scaledValue.toFixed(digitsAfterDecimalPoint) + (suffix || '');
-};
-
-// /*
-//  * Tests
-//  */
-// const testsNumber = [
-//     { num: 0, digits: 1 },
-//     { num: 0.000000001, digits: 1 },
-//     { num: 0.163456, digits: 1 },
-//     { num: 12, digits: 1 },
-//     { num: 1234, digits: 1 },
-//     { num: 100000000, digits: 1 },
-//     { num: 299792458, digits: 1 },
-//     { num: 299792458123124, digits: 1 },
-//     { num: 759878, digits: 1 },
-//     { num: 759878, digits: 0 },
-//     { num: 123, digits: 1 },
-//     { num: 123.456, digits: 1 },
-//     { num: 123.456, digits: 2 },
-//     { num: 123.456, digits: 4 },
-// ];
-// const testsBigNumber = [
-//     { num: new BigNumber(0), digits: 1 },
-//     { num: new BigNumber(0.000000001), digits: 1 },
-//     { num: new BigNumber(0.163456), digits: 1 },
-//     { num: new BigNumber(12), digits: 1 },
-//     { num: new BigNumber(1234), digits: 1 },
-//     { num: new BigNumber(100000000), digits: 1 },
-//     { num: new BigNumber(299792458), digits: 1 },
-//     { num: new BigNumber(-299792458), digits: 1 },
-//     { num: new BigNumber('299792458123124123123123'), digits: 1 },
-//     { num: new BigNumber(759878), digits: 1 },
-//     { num: new BigNumber(759878), digits: 0 },
-//     { num: new BigNumber(123), digits: 1 },
-//     { num: new BigNumber(123.456), digits: 1 },
-//     { num: new BigNumber(123.456), digits: 2 },
-//     { num: new BigNumber(-123.456), digits: 2 },
-//     { num: new BigNumber(123.456), digits: 4 },
-// ];
-// console.log();
-// testsBigNumber.forEach((test) => {
-//     console.log(
-//         `numberFormat(${test.num}, ${test.digits}) = ${numberFormat(test.num, {
-//             digitsAfterDecimalPoint: test.digits,
-//         })}`,
-//     );
-// });
-// console.log();
