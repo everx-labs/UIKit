6c1785ab3785f3270ad2631d91e9a18d2303fed5
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-06-22 18:34:04 +0300

    Move types into src.

--- packages/charts/src/index.ts
+++ packages/charts/src/index.ts
@@ -1,4 +1,4 @@
 export { LinearChart } from './Linear';
-export * from './types'
-export * from './Utils'
-export * from './constants'
+export * from './types';
+export * from './Utils';
+export * from './constants';
