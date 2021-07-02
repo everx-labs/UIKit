547a89e56fadf513d1187ff9873d4db67d6c2533
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-07-02 10:45:30 +0300

    Add nice UIQRCodeView.

--- packages/hydrogen/src/makeStyles.ts
+++ packages/hydrogen/src/makeStyles.ts
@@ -10,6 +10,30 @@ type MakeStyles = <T extends Record<string, unknown>>(
     styles: NamedStyles<T> | FunctionStyles<T>,
 ) => (...args: unknown[]) => NamedStyles<T>;
 
+/**
+ * This function is used to dynamically style the React component.
+ *
+ * Usecase: {
+ *      Make a hook:
+ *      const useStyles = makeStyles((theme, dynamicWidth) => ({
+ *          container: {
+ *              width: dynamicWidth,
+ *              backgroundColor: theme[ColorVariants.BackgroundPrimary],
+ *          },
+ *      }));
+ *
+ *      const component: React.FC = (props) => {
+ *          const theme = useTheme();
+ *          const dynamicWidth = /Some calculation e.g. from props.width/;
+ *          const styles = useStyles(theme, dynamicWidth);
+ *
+ *          return <View style={styles.container}>;
+ *      };
+ * }
+ *
+ * @param styles Can be a style `object` or a `function` that returns a style object
+ * @returns A hook that accepts arguments that it passes as parameters to the `styles` function
+ */
 export const makeStyles: MakeStyles = (styles) => (...args) => {
     return React.useMemo(() => {
         if (typeof styles === 'function') {
