219ad7ba71b93c8ad3b9b2bf9e2d58092bc82548
Aleksei Saveliev <savelichalex93@gmail.com>
2021-07-22 15:30:41 +0300

    Use fallback for iphone6s taptic engine

--- packages/hydrogen/ios/HHapticModule.mm
+++ packages/hydrogen/ios/HHapticModule.mm
@@ -5,7 +5,11 @@
 //  Created by Aleksei Saveliev on 21.07.2021.
 //
 
+#import <AudioToolbox/AudioToolbox.h>
+#import <AudioToolbox/AudioServices.h>
 #import <Foundation/Foundation.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 
 #import <React/RCTBridge+Private.h>
 #import <React/RCTUtils.h>
@@ -23,139 +27,169 @@
 RCT_EXPORT_MODULE()
 
 + (BOOL)requiresMainQueueSetup {
-  return YES;
+    return YES;
 }
 
 - (void)setBridge:(RCTBridge *)bridge
 {
-  _bridge = bridge;
-  
-  // TODO(savelichalex): a little temporary hack to give time for runtime to initialize
-  [[NSNotificationCenter defaultCenter] addObserver:self
-      selector:@selector(jsLoaded:)
-          name:RCTJavaScriptDidLoadNotification
-        object:nil];
+    _bridge = bridge;
+    
+    // TODO(savelichalex): a little temporary hack to give time for runtime to initialize
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(jsLoaded:)
+                                                 name:RCTJavaScriptDidLoadNotification
+                                               object:nil];
 }
 
 - (void)jsLoaded:(NSNotification *)notification {
-  RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
-  
-  if (!cxxBridge.runtime) {
-    return;
-  }
-  
-  jsi::Runtime *runtime = reinterpret_cast<facebook::jsi::Runtime *>(cxxBridge.runtime);
-  
-  jsi::Object reanimatedModuleProxy = runtime->global().getPropertyAsObject(*runtime, "__reanimatedModuleProxy");
-  std::shared_ptr<reanimated::NativeReanimatedModule> reanimatedModule = std::static_pointer_cast<reanimated::NativeReanimatedModule>(reanimatedModuleProxy.getHostObject(*runtime));
-  
-  jsi::Runtime &reanimatedUIRuntime = *reanimatedModule->runtime.get();
-  
-  HHapticModule *_this = self;
-  
-  auto hapticImpactCallback = [_this](
-                              jsi::Runtime& rt,
-                              const jsi::Value& thisVal,
-                              const jsi::Value *args,
-                              size_t count
-                              ) -> jsi::Value {
-    const auto inputStyle = args[0].asString(rt);
+    RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
     
-    [_this hapticImpact:[NSString stringWithUTF8String:inputStyle.utf8(rt).c_str()]];
+    if (!cxxBridge.runtime) {
+        return;
+    }
     
-    return jsi::Value::undefined();
-  };
-  
-  jsi::Value hapticImpact = jsi::Function::createFromHostFunction(
-                                                                  reanimatedUIRuntime,
-                                                                  jsi::PropNameID::forAscii(reanimatedUIRuntime, "_hapticImpact"),
-                                                                  1,
-                                                                  hapticImpactCallback);
-  
-  reanimatedUIRuntime.global().setProperty(reanimatedUIRuntime, "_hapticImpact", hapticImpact);
-  
-  auto hapticSelectionCallback = [_this](
-                              jsi::Runtime& rt,
-                              const jsi::Value& thisVal,
-                              const jsi::Value *args,
-                              size_t count
-                              ) -> jsi::Value {
-    [_this hapticSelection];
+    jsi::Runtime *runtime = reinterpret_cast<facebook::jsi::Runtime *>(cxxBridge.runtime);
     
-    return jsi::Value::undefined();
-  };
-  
-  jsi::Value hapticSelection = jsi::Function::createFromHostFunction(
-                                                                  reanimatedUIRuntime,
-                                                                  jsi::PropNameID::forAscii(reanimatedUIRuntime, "_hapticSelection"),
-                                                                  0,
-                                                                  hapticSelectionCallback);
-  
-  reanimatedUIRuntime.global().setProperty(reanimatedUIRuntime, "_hapticSelection", hapticSelection);
-  
-  auto hapticNotificationCallback = [_this](
-                              jsi::Runtime& rt,
-                              const jsi::Value& thisVal,
-                              const jsi::Value *args,
-                              size_t count
-                              ) -> jsi::Value {
-    const auto inputType = args[0].asString(rt);
+    jsi::Object reanimatedModuleProxy = runtime->global().getPropertyAsObject(*runtime, "__reanimatedModuleProxy");
+    std::shared_ptr<reanimated::NativeReanimatedModule> reanimatedModule = std::static_pointer_cast<reanimated::NativeReanimatedModule>(reanimatedModuleProxy.getHostObject(*runtime));
     
-    [_this hapticNotification:[NSString stringWithUTF8String:inputType.utf8(rt).c_str()]];
+    jsi::Runtime &reanimatedUIRuntime = *reanimatedModule->runtime.get();
     
-    return jsi::Value::undefined();
-  };
-  
-  jsi::Value hapticNotification = jsi::Function::createFromHostFunction(
-                                                                  reanimatedUIRuntime,
-                                                                  jsi::PropNameID::forAscii(reanimatedUIRuntime, "_hapticNotification"),
-                                                                  1,
-                                                                  hapticNotificationCallback);
-  
-  reanimatedUIRuntime.global().setProperty(reanimatedUIRuntime, "_hapticNotification", hapticNotification);
+    HHapticModule *_this = self;
+    
+    auto hapticImpactCallback = [_this](
+                                        jsi::Runtime& rt,
+                                        const jsi::Value& thisVal,
+                                        const jsi::Value *args,
+                                        size_t count
+                                        ) -> jsi::Value {
+        const auto inputStyle = args[0].asString(rt);
+        
+        [_this hapticImpact:[NSString stringWithUTF8String:inputStyle.utf8(rt).c_str()]];
+        
+        return jsi::Value::undefined();
+    };
+    
+    jsi::Value hapticImpact = jsi::Function::createFromHostFunction(
+                                                                    reanimatedUIRuntime,
+                                                                    jsi::PropNameID::forAscii(reanimatedUIRuntime, "_hapticImpact"),
+                                                                    1,
+                                                                    hapticImpactCallback);
+    
+    reanimatedUIRuntime.global().setProperty(reanimatedUIRuntime, "_hapticImpact", hapticImpact);
+    
+    auto hapticSelectionCallback = [_this](
+                                           jsi::Runtime& rt,
+                                           const jsi::Value& thisVal,
+                                           const jsi::Value *args,
+                                           size_t count
+                                           ) -> jsi::Value {
+        [_this hapticSelection];
+        
+        return jsi::Value::undefined();
+    };
+    
+    jsi::Value hapticSelection = jsi::Function::createFromHostFunction(
+                                                                       reanimatedUIRuntime,
+                                                                       jsi::PropNameID::forAscii(reanimatedUIRuntime, "_hapticSelection"),
+                                                                       0,
+                                                                       hapticSelectionCallback);
+    
+    reanimatedUIRuntime.global().setProperty(reanimatedUIRuntime, "_hapticSelection", hapticSelection);
+    
+    auto hapticNotificationCallback = [_this](
+                                              jsi::Runtime& rt,
+                                              const jsi::Value& thisVal,
+                                              const jsi::Value *args,
+                                              size_t count
+                                              ) -> jsi::Value {
+        const auto inputType = args[0].asString(rt);
+        
+        [_this hapticNotification:[NSString stringWithUTF8String:inputType.utf8(rt).c_str()]];
+        
+        return jsi::Value::undefined();
+    };
+    
+    jsi::Value hapticNotification = jsi::Function::createFromHostFunction(
+                                                                          reanimatedUIRuntime,
+                                                                          jsi::PropNameID::forAscii(reanimatedUIRuntime, "_hapticNotification"),
+                                                                          1,
+                                                                          hapticNotificationCallback);
+    
+    reanimatedUIRuntime.global().setProperty(reanimatedUIRuntime, "_hapticNotification", hapticNotification);
 }
 
 - (void)hapticImpact:(NSString *)inputStyle {
-  UIImpactFeedbackStyle style;
-  if ([inputStyle isEqualToString:@"light"]) {
-    style = UIImpactFeedbackStyleLight;
-  } else if ([inputStyle isEqualToString:@"medium"]) {
-    style = UIImpactFeedbackStyleMedium;
-  } else if ([inputStyle isEqualToString:@"heavy"]) {
-    style = UIImpactFeedbackStyleHeavy;
-  } else {
-    throw [NSException exceptionWithName:@"IncorrectInputStyle" reason:@"Haptic impact was called with incorrect input style" userInfo:nil];
-  }
-  
-  UIImpactFeedbackGenerator *feedback = [[UIImpactFeedbackGenerator alloc] initWithStyle:style];
-  [feedback prepare];
-  [feedback impactOccurred];
-  feedback = nil;
+    if ([self supportsHapticFor6SAnd6SPlus]) {
+        AudioServicesPlaySystemSound((SystemSoundID) 1520);
+        return;
+    }
+    
+    UIImpactFeedbackStyle style;
+    if ([inputStyle isEqualToString:@"light"]) {
+        style = UIImpactFeedbackStyleLight;
+    } else if ([inputStyle isEqualToString:@"medium"]) {
+        style = UIImpactFeedbackStyleMedium;
+    } else if ([inputStyle isEqualToString:@"heavy"]) {
+        style = UIImpactFeedbackStyleHeavy;
+    } else {
+        throw [NSException exceptionWithName:@"IncorrectInputStyle" reason:@"Haptic impact was called with incorrect input style" userInfo:nil];
+    }
+    
+    UIImpactFeedbackGenerator *feedback = [[UIImpactFeedbackGenerator alloc] initWithStyle:style];
+    [feedback prepare];
+    [feedback impactOccurred];
+    feedback = nil;
 }
 
 - (void)hapticSelection {
-  UISelectionFeedbackGenerator *feedback = [UISelectionFeedbackGenerator new];
-  [feedback prepare];
-  [feedback selectionChanged];
-  feedback = nil;
+    if ([self supportsHapticFor6SAnd6SPlus]) {
+        AudioServicesPlaySystemSound((SystemSoundID) 1519);
+        return;
+    }
+    
+    UISelectionFeedbackGenerator *feedback = [UISelectionFeedbackGenerator new];
+    [feedback prepare];
+    [feedback selectionChanged];
+    feedback = nil;
 }
 
 - (void)hapticNotification:(NSString *)inputType {
-  UINotificationFeedbackType type;
-  if ([inputType isEqualToString:@"success"]) {
-    type = UINotificationFeedbackTypeSuccess;
-  } else if ([inputType isEqualToString:@"warning"]) {
-    type = UINotificationFeedbackTypeWarning;
-  } else if ([inputType isEqualToString:@"error"]) {
-    type = UINotificationFeedbackTypeError;
-  } else {
-    throw [NSException exceptionWithName:@"IncorrectInputType" reason:@"Haptic notification was called with incorrect input type" userInfo:nil];
-  }
-  
-  UINotificationFeedbackGenerator *feedback = [UINotificationFeedbackGenerator new];
-  [feedback prepare];
-  [feedback notificationOccurred:type];
-  feedback = nil;
+    if ([self supportsHapticFor6SAnd6SPlus]) {
+        AudioServicesPlaySystemSound((SystemSoundID) 1521);
+        return;
+    }
+    
+    UINotificationFeedbackType type;
+    if ([inputType isEqualToString:@"success"]) {
+        type = UINotificationFeedbackTypeSuccess;
+    } else if ([inputType isEqualToString:@"warning"]) {
+        type = UINotificationFeedbackTypeWarning;
+    } else if ([inputType isEqualToString:@"error"]) {
+        type = UINotificationFeedbackTypeError;
+    } else {
+        throw [NSException exceptionWithName:@"IncorrectInputType" reason:@"Haptic notification was called with incorrect input type" userInfo:nil];
+    }
+    
+    UINotificationFeedbackGenerator *feedback = [UINotificationFeedbackGenerator new];
+    [feedback prepare];
+    [feedback notificationOccurred:type];
+    feedback = nil;
+}
+
+-(Boolean)supportsHapticFor6SAnd6SPlus {
+    return ([[self platform] isEqualToString:@"iPhone8,1"]  // iPhone 6S
+        || [[self platform] isEqualToString:@"iPhone8,2"]); // iPhone 6S Plus
+}
+
+- (NSString *)platform {
+    size_t size;
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    char *machine = (char *)malloc(size);
+    sysctlbyname("hw.machine", machine, &size, NULL, 0);
+    NSString *platform = [NSString stringWithUTF8String:machine];
+    free(machine);
+    return platform;
 }
 
 @end
