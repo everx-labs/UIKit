a0065ff777380d69a2698530837e7086fd135e88
Marina <wearedusa@icloud.com>
2021-08-03 19:40:22 +0300

    Formatted

--- packages/hydrogen/src/UISwitcher/RawButton.tsx
+++ packages/hydrogen/src/UISwitcher/RawButton.tsx
@@ -1,13 +1,16 @@
 import type * as React from 'react';
-import Animated from "react-native-reanimated";
-import type { NativeViewGestureHandlerProps, RawButtonProps } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+import type {
+    NativeViewGestureHandlerProps,
+    RawButtonProps,
+} from 'react-native-gesture-handler';
 import type { StyleProp, ViewStyle } from 'react-native';
 import { RawButton as GHRawButton } from 'react-native-gesture-handler/lib/typescript/components/GestureButtons';
 
 export const RawButton: React.FunctionComponent<Animated.AnimateProps<
     RawButtonProps &
-    NativeViewGestureHandlerProps & {
-    testID?: string;
-    style?: StyleProp<ViewStyle>;
-}
-    >> = Animated.createAnimatedComponent(GHRawButton);
+        NativeViewGestureHandlerProps & {
+            testID?: string;
+            style?: StyleProp<ViewStyle>;
+        }
+>> = Animated.createAnimatedComponent(GHRawButton);
