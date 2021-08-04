9c18cca52224dfcf0d483af1c515f96024e0fce7
Marina <wearedusa@icloud.com>
2021-08-04 10:34:06 +0300

    Fix path

--- packages/hydrogen/src/UISwitcher/RawButton.tsx
+++ packages/hydrogen/src/UISwitcher/RawButton.tsx
@@ -1,11 +1,11 @@
 import type * as React from 'react';
 import Animated from 'react-native-reanimated';
-import type {
+import {
+    RawButton as GHRawButton,
     NativeViewGestureHandlerProps,
     RawButtonProps,
 } from 'react-native-gesture-handler';
 import type { StyleProp, ViewStyle } from 'react-native';
-import { RawButton as GHRawButton } from 'react-native-gesture-handler';
 
 export const RawButton: React.FunctionComponent<Animated.AnimateProps<
     RawButtonProps &
