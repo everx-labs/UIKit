c97c3c38bd45f764e86791a64cfa739991d6e9ef
sertony <serkov_anton@mail.ru>
2021-08-06 14:25:05 +0300

    fix the localized-strings patch

--- Example/postinstall.sh
+++ Example/postinstall.sh
@@ -1,2 +1,4 @@
 ./node_modules/.bin/jetify
-node ../scripts/localizedStringsPatch.js
+
+# Fix `localized-strings` to make it work with Lokalize.com
+sed -i '' -e '195s/.*/      } else if (typeof strings[key] !== "string" \&\& typeof strings[key].valueOf() !== "string") {/' ../node_modules/localized-strings/lib/LocalizedStrings.js
