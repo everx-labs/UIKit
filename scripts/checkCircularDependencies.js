d8cebfd446c524a9f340989d382db2cd9880fece
Anton S <serkov_anton@mail.ru>
2021-08-09 10:35:26 +0300

    Dpdm (#1193)
    * add DPDM circular deps check
    
    * add DPDM circular deps check
    
    * add more sources to script
    
    * fix dpdm version

--- scripts/checkCircularDependencies.js
+++ scripts/checkCircularDependencies.js
@@ -1,8 +1,45 @@
 const madge = require('madge');
 const path = require('path');
+const { parseDependencyTree, parseCircular, prettyCircular } = require('dpdm');
 
 const checkCircularDependencies = async () => {
     try {
+        console.log('Testing for circular dependencies using "DPDM"...');
+        const sources = [
+            'index.js',
+            'Example/index.js',
+            'packages/assets/src/index.ts',
+            'packages/browser/src/index.ts',
+            'packages/charts/src/index.ts',
+            'packages/chats/src/index.ts',
+            'packages/components/src/index.js',
+            'packages/core/src/index.js',
+            'packages/flask/src/index.ts',
+            'packages/hydrogen/src/index.ts',
+            'packages/keyboard/src/index.ts',
+            'packages/legacy/src/index.js',
+            'packages/localization/src/index.ts',
+            'packages/navigation/src/index.ts',
+            'packages/navigation_legacy/src/index.js',
+            'packages/stickers/src/index.ts',
+        ];
+        const tree = await parseDependencyTree(sources, { /* default options */ });
+        const circulars = parseCircular(tree);
+        if (circulars.length > 0) {
+            console.log(
+                '\x1b[33m%s\x1b[0m', // yellow
+                `Circular dependencies were found by "DPDM" (${circulars.length})[TODO: fix them!!!]:\n`,
+                prettyCircular(circulars),
+            );
+            // process.exit(1); Uncomment once ready!!!
+        } else {
+            console.info(
+                '\x1b[32m%s\x1b[0m', // green
+                'Congratulations! "DPDM" haven\'t found any circular dependencies in your code!',
+            );
+        }
+
+        console.log('Testing for circular dependencies using "Madge"...');
         const res = await madge(path.join(__dirname, '..'), {
             fileExtensions: ['js', 'jsx', 'ts', 'tsx'],
         });
@@ -10,13 +47,14 @@ const checkCircularDependencies = async () => {
         if (dependencies.length > 0) {
             console.log(
                 '\x1b[33m%s\x1b[0m', // yellow
-                `Circular dependencies were found (${dependencies.length}):\n`, dependencies,
+                `Circular dependencies were found by "Madge" (${dependencies.length}):\n`,
+                dependencies,
             );
             process.exit(1);
         } else {
             console.info(
-                '\x1b[34m%s\x1b[0m', // blue
-                'Congratulations! Your code doesn\'t have any circular dependencies!',
+                '\x1b[32m%s\x1b[0m', // green
+                'Congratulations! "Madge" haven\'t found any circular dependencies in your code!',
             );
         }
     } catch (error) {
@@ -24,6 +62,7 @@ const checkCircularDependencies = async () => {
             '\x1b[31m%s\x1b[0m', // red
             'Failed to find circular dependencies with error:', error,
         );
+        process.exit(1);
     }
 };
 
