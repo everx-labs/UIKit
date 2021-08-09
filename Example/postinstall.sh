bec43647bb6d86c8d81ac427334ddc5443821e49
sertony <serkov_anton@mail.ru>
2021-08-09 17:51:14 +0300

    move sed calls above in postinstall.sh script

--- Example/postinstall.sh
+++ Example/postinstall.sh
@@ -1,4 +1,5 @@
-./node_modules/.bin/jetify
-
 # Fix `localized-strings` to make it work with Lokalize.com
 sed -i '' -e '195s/.*/      } else if (typeof strings[key] !== "string" \&\& typeof strings[key].valueOf() !== "string") {/' ../node_modules/localized-strings/lib/LocalizedStrings.js
+
+# Jetify Android dependencies
+./node_modules/.bin/jetify
