0d23886e89ae3939562da5da29388c488c3b1c13
Anton S <serkov_anton@mail.ru>
2021-08-12 17:59:22 +0300

    React native 0.64 (#1201)
    * update major files
    
    * fix dependencies
    
    * fix pods
    
    * make it run on iOS
    
    * fix paths
    
    * fix dependencies
    
    * update types/jest
    
    * fix mocks
    
    * increase gradle version
    
    * enableeHermes for Android

--- Example/webpack.config.prod.js
+++ Example/webpack.config.prod.js
@@ -201,7 +201,7 @@ module.exports = {
                 'react-native-web/dist/exports/Text/TextAncestorContext',
             'react-native/Libraries/Components/View/ReactNativeStyleAttributes':
                 'react',
-            'react-native/Libraries/Animated/src/SpringConfig':
+            'react-native/Libraries/Animated/SpringConfig':
                 'react-native-web/dist/vendor/react-native/Animated/SpringConfig',
             'react-native$': 'react-native-web',
             'react-native-safe-area': 'react', // Hack in order not to load
