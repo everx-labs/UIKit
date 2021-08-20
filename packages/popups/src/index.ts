8a87fc86a36efb2cf79094eb2f34e0c238e9321c
Rina <71766968+wearedusa@users.noreply.github.com>
2021-08-20 14:53:30 +0300

    Export types of popups (#1222)

--- packages/popups/src/index.ts
+++ packages/popups/src/index.ts
@@ -2,6 +2,10 @@ import { IUIAlertView, UIAlertView } from './UIAlertView';
 import { IUIActionSheet, UIActionSheet } from './UIActionSheet';
 import { IUINotice, UINotice } from './UINotice';
 
+export * from './Notice/types';
+export * from './ActionSheet/types';
+export * from './UIAlert/types';
+
 const Notice: IUINotice = UINotice;
 const ActionSheet: IUIActionSheet = UIActionSheet;
 const AlertView: IUIAlertView = UIAlertView;
