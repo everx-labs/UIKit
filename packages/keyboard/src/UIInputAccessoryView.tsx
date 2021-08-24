09fed6e39d0b960388394a6b0c5966d8cd1ab174
Aleksei Saveliev <savelichalex93@gmail.com>
2021-08-24 14:06:56 +0300

    Remove usage of UIDevice.safeAreaInsets() from stickers view

--- packages/keyboard/src/UIInputAccessoryView.tsx
+++ packages/keyboard/src/UIInputAccessoryView.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import {
     InputAccessoryView,
     // @ts-ignore
-    // eslint-disable-next-line import/no-unresolved
+    // eslint-disable-next-line import/no-unresolved, import/extensions
 } from './InputAccessoryView';
 import type { UIInputAccessoryViewProps } from './types';
 
