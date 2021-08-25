11880bf5c646b652fd26ee6bbda6760275f45741
sertony <serkov_anton@mail.ru>
2021-08-25 12:41:03 +0300

    remove unneeded dev dependencies

--- packages/flask/babel.config.js
+++ packages/flask/babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: { node: '10' },
-            },
-        ],
         'module:metro-react-native-babel-preset',
     ],
     plugins: ['@tonlabs/babel-plugin-transform-inline-consts', 'react-native-reanimated/plugin'],
--- packages/hydrogen/babel.config.js
+++ packages/hydrogen/babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: { node: '10' },
-            },
-        ],
         'module:metro-react-native-babel-preset',
     ],
     plugins: ['react-native-reanimated/plugin'],
