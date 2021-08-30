0589a419541ae32eb071778ebe635b2d3cbbbce0
Anton S <serkov_anton@mail.ru>
2021-08-30 19:13:02 +0300

    add application release keystore for android (#1240)
    * add application release keystore for android
    
    * share secret
    
    * improve readme
    
    * improve reinstall script
    
    * make it work in release on Android
    
    * fix README

--- Example/reinstall.sh
+++ Example/reinstall.sh
@@ -1,6 +1,19 @@
 npx lerna bootstrap
 npx lerna run prepare
+
 cd ios
 rm -rf Pods
 pod install
 cd ..
+
+if command -v osascript &> /dev/null
+then
+  osascript -e "display notification \"Waiting for secret files revealing\" with title \"Quiver UI\""
+fi
+
+npm run secret:reveal
+
+if command -v osascript &> /dev/null
+then
+  osascript -e "display notification \"Reinstalled!\" with title \"Quiver UI\""
+fi
