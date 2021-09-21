e8497c32a91988b63ce3ab0ed235359012edccd1
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-09-21 17:11:24 +0300

    New structure.

--- kit/themes/src/index.ts
+++ kit/themes/src/index.ts
@@ -1,4 +1,8 @@
 import * as Colors from './Colors';
+import * as Typography from './Typography';
+import * as UIBackgroundView from './UIBackgroundView';
+import * as UILabel from './UILabel';
+import * as useWebFonts from './useWebFonts';
 
 export * from './useWebFonts';
 export * from './Colors';
@@ -7,5 +11,9 @@ export * from './UIBackgroundView';
 export * from './UILabel';
 export const UIThemes = {
     Colors,
+    Typography,
+    UIBackgroundView,
+    UILabel,
+    useWebFonts,
 };
 export default UIThemes;
