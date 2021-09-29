77926dabbcc3ef647dae6455faaca0404a197431
Marina <wearedusa@icloud.com>
2021-09-29 19:38:36 +0300

    New datetimepicker controls

---
+++ packages/flask/src/UIDateTimePicker/datePicker/components/Controls.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { View } from 'react-native';
+import {
+    ColorVariants,
+    makeStyles,
+    Theme,
+    TouchableOpacity,
+    TypographyVariants,
+    UILabel,
+    useTheme,
+} from '@tonlabs/uikit.hydrogen';
+
+export type DateControlsProps = {
+    onPressMonth?: () => void;
+    onPressYear?: () => void;
+    month?: string;
+    year?: string;
+};
+
+export function Separator() {
+    const theme = useTheme();
+    const styles = useStyles(theme);
+    return <View style={styles.separator} />;
+}
+
+export function MonthYearButton({ onPressMonth, onPressYear, month, year }: DateControlsProps) {
+    const theme = useTheme();
+    const styles = useStyles(theme);
+
+    return (
+        <View style={styles.monthYearContainer}>
+            {!!month && (
+                <TouchableOpacity onPress={onPressMonth}>
+                    <UILabel role={TypographyVariants.Action}>{month}</UILabel>
+                </TouchableOpacity>
+            )}
+            {!!month && !!year && <Separator />}
+            {!!year && (
+                <TouchableOpacity onPress={onPressYear}>
+                    <UILabel role={TypographyVariants.Action}>{year}</UILabel>
+                </TouchableOpacity>
+            )}
+        </View>
+    );
+}
+
+export type ArrowsButtonsProps = {
+    onPressLeft: () => void;
+    onPressRight: () => void;
+};
+
+export function ArrowsButtons({ onPressLeft, onPressRight }: ArrowsButtonsProps) {
+    const theme = useTheme();
+    const styles = useStyles(theme);
+
+    return (
+        <View>
+            <View>
+                <View style={styles.monthYearContainer}>
+                    <TouchableOpacity onPress={onPressLeft}>
+                        <UILabel role={TypographyVariants.Action}>{'<'}</UILabel>
+                    </TouchableOpacity>
+                    <Separator />
+                    <TouchableOpacity onPress={onPressRight}>
+                        <UILabel role={TypographyVariants.Action}>{'>'}</UILabel>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+    monthYearContainer: {
+        backgroundColor: theme[ColorVariants.BackgroundTertiary] as string,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+    },
+    separator: {
+        height: 12,
+        width: 1,
+        backgroundColor: theme[ColorVariants.BackgroundTertiaryInverted],
+        marginHorizontal: 12,
+    },
+}));
--- packages/flask/src/UIDateTimePicker/datePicker/components/Header.tsx
+++ packages/flask/src/UIDateTimePicker/datePicker/components/Header.tsx
@@ -1,216 +1,88 @@
 import React, { useState } from 'react';
-import { UIAssets } from '@tonlabs/uikit.assets';
-import {
-    View,
-    TouchableOpacity,
-    Text,
-    Image,
-    StyleSheet,
-    Animated,
-    I18nManager,
-} from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { makeStyles, TypographyVariants, UILabel, useTheme } from '@tonlabs/uikit.hydrogen';
 
 import { useCalendar } from '../calendarContext';
+import { ArrowsButtons, MonthYearButton } from './Controls';
 
 export const Header = ({ changeMonth }: any) => {
     const { options, disableDateChange, state, utils, min, max, mode } = useCalendar();
     const [mainState, setMainState] = state;
-    const style = styles(options);
+    const theme = useTheme();
+    const styles = useStyles(theme);
     const [disableChange, setDisableChange] = useState(false);
-    const [{ lastDate, shownAnimation, hiddenAnimation }, changeMonthAnimation] =
-        utils.useMonthAnimation(
-            mainState.activeDate,
-            options && options.headerAnimationDistance,
-            () => setDisableChange(false),
-        );
+    const [{ lastDate }, changeMonthAnimation] = utils.useMonthAnimation(
+        mainState.activeDate,
+        options && options.headerAnimationDistance,
+        () => setDisableChange(false),
+    );
     const prevDisable =
         disableDateChange || (min && utils.checkArrowMonthDisabled(mainState.activeDate, true));
     const nextDisable =
         disableDateChange || (max && utils.checkArrowMonthDisabled(mainState.activeDate, false));
 
-    const onChangeMonth = (type: string) => {
-        if (disableChange) return;
-        setDisableChange(true);
-        changeMonthAnimation(type);
-        const modificationNumber = type === 'NEXT' ? 1 : -1;
-        const newDate = utils.getDate(mainState.activeDate).add(modificationNumber, 'month');
-        setMainState({
-            type: 'set',
-            activeDate: utils.getFormatted(newDate),
-        });
-        changeMonth(type);
-    };
+    const onChangeMonth = React.useCallback(
+        (type: string) => {
+            if (disableChange) return;
+            setDisableChange(true);
+            changeMonthAnimation(type);
+            const modificationNumber = type === 'NEXT' ? 1 : -1;
+            const newDate = utils.getDate(mainState.activeDate).add(modificationNumber, 'month');
+            setMainState({
+                type: 'set',
+                activeDate: utils.getFormatted(newDate),
+            });
+            changeMonth(type);
+        },
+        [
+            changeMonth,
+            changeMonthAnimation,
+            disableChange,
+            mainState.activeDate,
+            setMainState,
+            utils,
+        ],
+    );
+
+    const onPrevMonth = React.useCallback(() => {
+        !prevDisable && onChangeMonth('PREVIOUS');
+    }, [onChangeMonth, prevDisable]);
+
+    const onNextMonth = React.useCallback(() => {
+        !nextDisable && onChangeMonth('NEXT');
+    }, [onChangeMonth, nextDisable]);
+
+    const onPressMonth = React.useCallback(() => {
+        !disableDateChange &&
+            setMainState({
+                type: 'toggleMonth',
+            });
+    }, [disableDateChange, setMainState]);
 
     return (
-        <View style={[style.container, I18nManager.isRTL && style.reverseContainer]}>
-            <TouchableOpacity
-                activeOpacity={0.7}
-                onPress={() => !nextDisable && onChangeMonth('NEXT')}
-                style={style.arrowWrapper}
-            >
-                <Image
-                    source={UIAssets.icons.ui.arrowLeft}
-                    style={[style.arrow, style.rightArrow, nextDisable && style.disableArrow]}
+        <View style={styles.container}>
+            <UILabel role={TypographyVariants.TitleMedium}>Title</UILabel>
+            <View style={styles.controlsContainer}>
+                <MonthYearButton
+                    onPressMonth={onPressMonth}
+                    onPressYear={() => console.log('todo: year view')}
+                    month={utils.getMonthYearText(lastDate).split(' ')[0]}
+                    year={utils.getMonthYearText(mainState.activeDate).split(' ')[1]}
                 />
-            </TouchableOpacity>
-            <View style={style.monthYearContainer}>
-                <Animated.View
-                    style={[
-                        style.monthYear,
-                        shownAnimation,
-                        style.activeMonthYear,
-                        I18nManager.isRTL && style.reverseMonthYear,
-                    ]}
-                >
-                    <TouchableOpacity
-                        activeOpacity={0.7}
-                        style={[style.centerWrapper, style.monthYearWrapper, utils.flexDirection]}
-                        onPress={() =>
-                            !disableDateChange &&
-                            setMainState({
-                                type: 'toggleMonth',
-                            })
-                        }
-                    >
-                        <Text style={[style.headerText, style.monthText]}>
-                            {utils.getMonthYearText(mainState.activeDate).split(' ')[0]}
-                        </Text>
-                        <Text style={[style.headerText, style.monthText]}>
-                            {utils.getMonthYearText(mainState.activeDate).split(' ')[1]}
-                        </Text>
-                    </TouchableOpacity>
-                    {mode === 'datepicker' && (
-                        <TouchableOpacity
-                            activeOpacity={0.7}
-                            style={[
-                                style.centerWrapper,
-                                {
-                                    marginRight: I18nManager.isRTL ? 0 : 5,
-                                    marginLeft: I18nManager.isRTL ? 5 : 0,
-                                },
-                            ]}
-                            onPress={() =>
-                                setMainState({
-                                    type: 'toggleTime',
-                                })
-                            }
-                        >
-                            <Text style={style.headerText}>
-                                {utils.getTime(
-                                    utils.returnValidTime(mainState.activeDate),
-                                    min,
-                                    max,
-                                )}
-                            </Text>
-                        </TouchableOpacity>
-                    )}
-                </Animated.View>
-                <Animated.View
-                    style={[
-                        style.monthYear,
-                        hiddenAnimation,
-                        utils.flexDirection,
-                        I18nManager.isRTL && style.reverseMonthYear,
-                    ]}
-                >
-                    <Text style={style.headerText}>
-                        {utils.getMonthYearText(lastDate).split(' ')[0]}
-                    </Text>
-                    <Text style={style.headerText}>
-                        {utils.getMonthYearText(lastDate).split(' ')[1]}
-                    </Text>
-                    {mode === 'datepicker' && (
-                        <Text style={style.headerText}>
-                            {utils.getTime(utils.returnValidTime(mainState.activeDate), min, max)}
-                        </Text>
-                    )}
-                </Animated.View>
+                <ArrowsButtons onPressRight={onPrevMonth} onPressLeft={onNextMonth} />
             </View>
-            <TouchableOpacity
-                activeOpacity={0.7}
-                onPress={() => !prevDisable && onChangeMonth('PREVIOUS')}
-                style={style.arrowWrapper}
-            >
-                <Image
-                    source={UIAssets.icons.ui.arrowLeft}
-                    style={[style.arrow, prevDisable && style.disableArrow]}
-                />
-            </TouchableOpacity>
         </View>
     );
 };
 
-const styles = (theme: any) =>
-    StyleSheet.create({
-        container: {
-            alignItems: 'center',
-            flexDirection: 'row-reverse',
-        },
-        reverseContainer: {
-            flexDirection: 'row',
-        },
-        arrowWrapper: {
-            padding: 20,
-            position: 'relative',
-            zIndex: 1,
-            opacity: 1,
-        },
-        arrow: {
-            width: 18,
-            height: 18,
-            opacity: 0.9,
-            tintColor: theme.mainColor,
-            margin: 2,
-        },
-        rightArrow: {
-            transform: [
-                {
-                    rotate: '180deg',
-                },
-            ],
-        },
-        disableArrow: {
-            opacity: 0,
-        },
-        monthYearContainer: {
-            flex: 1,
-            position: 'relative',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        monthYear: {
-            position: 'absolute',
-            alignItems: 'center',
-            flexDirection: 'row-reverse',
-        },
-        reverseMonthYear: {
-            flexDirection: 'row',
-        },
-        activeMonthYear: {
-            zIndex: 999,
-        },
-        monthYearWrapper: {
-            alignItems: 'center',
-        },
-        headerText: {
-            fontSize: theme.textHeaderFontSize,
-            padding: 2,
-            color: theme.textHeaderColor,
-            fontFamily: theme.defaultFont,
-            textAlignVertical: 'center',
-        },
-        monthText: {
-            fontFamily: theme.headerFont,
-        },
-        centerWrapper: {
-            borderColor: theme.borderColor,
-            paddingVertical: 4,
-            paddingHorizontal: 8,
-            alignItems: 'center',
-            borderRadius: 5,
-            borderWidth: 1,
-        },
-        time: {
-            marginRight: 5,
-        },
-    });
+const useStyles = makeStyles(() => ({
+    container: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 16,
+        justifyContent: 'space-between',
+    },
+    controlsContainer: {
+        flexDirection: 'row',
+    },
+}));
