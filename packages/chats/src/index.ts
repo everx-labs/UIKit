9b4aca159779787c576630bedd95ce5daa92c9af
vetkamak <vetka.mak@gmail.com>
2021-10-05 23:50:21 +0700

    export lightbox

--- packages/chats/src/index.ts
+++ packages/chats/src/index.ts
@@ -5,8 +5,10 @@ export * from './UIChatInput/ChatInputContainer';
 export * from './UIChatInput/useChatInputValue';
 export * from './BubbleQRCode';
 export * from './BubbleMedia';
+export * from './BubbleMedia/hooks';
 export * from './UILoadMoreButton';
 export * from './DuplicateImage';
+export * from './Lightbox/Lightbox';
 export { sectionListGetItemLayout } from './UIChatListLayout';
 
 export {
