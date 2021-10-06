0a88648df00416c362c29f9d310011d472027caf
vetkamak <vetka.mak@gmail.com>
2021-10-06 14:51:18 +0700

    add comment

--- packages/chats/src/index.ts
+++ packages/chats/src/index.ts
@@ -8,6 +8,8 @@ export * from './BubbleMedia';
 export * from './BubbleMedia/hooks';
 export * from './UILoadMoreButton';
 export * from './DuplicateImage';
+// TODO: as per the new UIKit architecture we need to move Lightbox
+//  to uikit.media once implemented
 export * from './Lightbox/Lightbox';
 export { sectionListGetItemLayout } from './UIChatListLayout';
 
