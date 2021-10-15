a92d308c38c352acd0764e3cc0bfee644b2bf27d
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-10-15 10:53:11 +0300

    Remove old packages.

--- packages/navigation/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-qr-reader';
-
-declare module '@tonlabs/uikit.core';
-declare module '@tonlabs/uikit.components';
-
-declare class WorkletEventHandler<Event> {
-    worklet: (event: Event) => void;
-}
