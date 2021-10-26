027b0fd872b7804ed3b7228dc177ef6b03600a0e
Marina <wearedusa@icloud.com>
2021-10-26 20:11:29 +0300

    Get rid of  react-native-country-picker-modal

--- Example/webpack.js
+++ Example/webpack.js
@@ -25,7 +25,6 @@ module.exports = {
                     path.resolve(__dirname, '../localization/'),
                     path.resolve(__dirname, '../node_modules/react-native-indicators/'),
                     path.resolve(__dirname, '../node_modules/react-native-simple-popover/'),
-                    path.resolve(__dirname, '../node_modules/react-native-country-picker-modal/'),
                     path.resolve(__dirname, '../node_modules/react-native-awesome-alerts/'),
                     path.resolve(__dirname, '../node_modules/react-native-parsed-text/'),
                     path.resolve(__dirname, '../node_modules/react-native-fast-image/'),
