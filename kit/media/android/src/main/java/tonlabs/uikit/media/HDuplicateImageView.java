f3b5921e5a2f75a0001d96573d502843b5ea9b7c
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-10-28 18:04:54 +0300

    Add lightbox gif for android.

--- kit/media/android/src/main/java/tonlabs/uikit/media/HDuplicateImageView.java
+++ kit/media/android/src/main/java/tonlabs/uikit/media/HDuplicateImageView.java
@@ -2,38 +2,26 @@ package tonlabs.uikit.media;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
 import android.widget.ImageView;
 import androidx.annotation.Nullable;
-import com.facebook.common.executors.CallerThreadExecutor;
-import com.facebook.common.references.CloseableReference;
-import com.facebook.datasource.DataSource;
-import com.facebook.imagepipeline.core.ImagePipeline;
-import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
-import com.facebook.imagepipeline.image.CloseableImage;
-import com.facebook.imagepipeline.request.ImageRequest;
-import com.facebook.imagepipeline.request.ImageRequestBuilder;
-import com.facebook.react.modules.fresco.FrescoModule;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.UIManagerModule;
-import com.facebook.react.views.image.ReactImageView;
-import com.facebook.react.views.imagehelper.ImageSource;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import com.facebook.react.views.view.ReactViewGroup;
 
 @SuppressLint("AppCompatCustomView")
 public class HDuplicateImageView extends ImageView {
 
     private final ThemedReactContext context;
     private final UIManagerModule uiManagerModule;
-    private final FrescoModule frescoModule;
 
     public HDuplicateImageView(Context context) {
         super(context);
         this.context = (ThemedReactContext) context;
         uiManagerModule = this.context.getNativeModule(UIManagerModule.class);
-        frescoModule = this.context.getNativeModule(FrescoModule.class);
     }
 
     void setOriginalViewRef(int originalViewRef) {
@@ -42,63 +30,43 @@ public class HDuplicateImageView extends ImageView {
                 if (uiManagerModule == null) {
                     return;
                 }
-                ReactImageView view = (ReactImageView) uiManagerModule.resolveView(originalViewRef);
+
                 try {
-                    Field imageSourceField = view.getClass().getDeclaredField("mImageSource");
-                    imageSourceField.setAccessible(true);
-                    ImageSource imageSource = (ImageSource) imageSourceField.get(view);
+                    @Nullable
+                    ImageView imageView = null;
 
-                    Method frescoModuleGetImagePipelineMethod =
-                        FrescoModule.class.getDeclaredMethod("getImagePipeline");
-                    frescoModuleGetImagePipelineMethod.setAccessible(true);
+                    View originalView = uiManagerModule.resolveView(originalViewRef);
+                    if (originalView instanceof ReactViewGroup) {
+                        ReactViewGroup reactViewGroup = (ReactViewGroup) originalView;
+                        View view = reactViewGroup.getChildAt(0);
+                        imageView = view instanceof ImageView ? ((ImageView) view) : null;
+                    } else if (originalView instanceof ImageView) {
+                        imageView = (ImageView) originalView;
+                    }
 
-                    ImagePipeline imagePipeline = (ImagePipeline) frescoModuleGetImagePipelineMethod.invoke(
-                        frescoModule
-                    );
-
-                    if (
-                        imagePipeline == null ||
-                        imageSource == null ||
-                        !imagePipeline.isInBitmapMemoryCache(imageSource.getUri())
-                    ) {
+                    if (imageView == null) {
                         return;
                     }
 
-                    ImageRequest imageRequest = ImageRequestBuilder
-                        .newBuilderWithSource(imageSource.getUri())
-                        .build();
+                    Drawable drawable = imageView
+                        .getDrawable()
+                        .getConstantState()
+                        .newDrawable()
+                        .mutate();
 
-                    DataSource<CloseableReference<CloseableImage>> dataSource = imagePipeline.fetchDecodedImage(
-                        imageRequest,
-                        context
-                    );
+                    if (drawable instanceof BitmapDrawable) {
+                        // Static image
+                        BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+                        setImageDrawable(bitmapDrawable);
+                    }
 
-                    BaseBitmapDataSubscriber baseBitmapDataSubscriber = new BaseBitmapDataSubscriber() {
-                        @Override
-                        protected void onNewResultImpl(@Nullable Bitmap bitmap) {
-                            if (bitmap == null) {
-                                return;
-                            }
-
-                            setImageBitmap(bitmap);
-                        }
-
-                        @Override
-                        protected void onFailureImpl(DataSource dataSource) {
-                            // Nothing to do
-                        }
-                    };
-
-                    CallerThreadExecutor callerThreadExecutor = CallerThreadExecutor.getInstance();
-
-                    dataSource.subscribe(baseBitmapDataSubscriber, callerThreadExecutor);
-                } catch (
-                    NoSuchFieldException
-                    | IllegalAccessException
-                    | NoSuchMethodException
-                    | InvocationTargetException
-                    | NullPointerException e
-                ) {
+                    if (drawable instanceof GifDrawable) {
+                        // Gif
+                        GifDrawable gifDrawable = (GifDrawable) drawable;
+                        gifDrawable.start();
+                        setImageDrawable(gifDrawable);
+                    }
+                } catch (NullPointerException e) {
                     e.printStackTrace();
                 }
             }
