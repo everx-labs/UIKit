4961b04fc84693a8677bd626496e0481e3349de0
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-11-01 12:22:54 +0300

    Add gif to lightbox.

--- stories/chats/src/BubbleMedia/hooks.tsx
+++ stories/chats/src/BubbleMedia/hooks.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Image, useWindowDimensions } from 'react-native';
+import { Image, Platform, useWindowDimensions } from 'react-native';
 import { UIImage } from '@tonlabs/uikit.media';
 import type { ImageSize } from './types';
 import { MediaMessageError } from '../types';
@@ -85,6 +85,20 @@ export const getImage = (
     if (!sourceUri) {
         return null;
     }
+
+    /** TODO Delete when lightbox for ios is made compatible with UIImage */
+    if (Platform.OS === 'ios') {
+        return (
+            <Image
+                ref={imageRef}
+                source={{ uri: sourceUri }}
+                style={imageSize}
+                onError={onErrorCallback}
+                onLoad={onLoadCallback}
+            />
+        );
+    }
+
     return (
         <UIImage
             ref={imageRef}
