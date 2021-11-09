fda0f43bddc60383c0f92b054bf624ceaba58b1a
Marina <wearedusa@icloud.com>
2021-11-09 17:54:34 +0300

    Fix

--- stories/browser/src/Inputs/menu.tsx
+++ stories/browser/src/Inputs/menu.tsx
@@ -14,12 +14,12 @@ import type { MenuMessage } from '../types';
 const ITEMS_TO_FOLD_COUNT = 5;
 
 export function MenuInput({ onLayout, ...message }: MenuMessage) {
-    const isItemsToFold = React.useMemo(
+    const hasItemsToFold = React.useMemo(
         () => message.items.length - ITEMS_TO_FOLD_COUNT < 2,
         [message.items.length],
     );
 
-    const [unfolded, setUnfolded] = React.useState(isItemsToFold);
+    const [unfolded, setUnfolded] = React.useState(hasItemsToFold);
 
     if (message.externalState != null) {
         return (
@@ -76,7 +76,7 @@ export function MenuInput({ onLayout, ...message }: MenuMessage) {
                     lastFromChain={unfolded && index === message.items.length - 1}
                 />
             ))}
-            {!unfolded && !isItemsToFold && (
+            {!unfolded && !hasItemsToFold && (
                 <BubbleActionButton
                     type={ChatMessageType.ActionButton}
                     key="menu-more"
