2cbbc96f853937acbd80a309ecc8f18f59898bee
Marina <wearedusa@icloud.com>
2021-11-17 18:35:23 +0300

    Removing

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -5,7 +5,6 @@ export { default as UIDetailsTable } from './UIDetailsTable';
 export { default as UIDetailsView } from './UIDetailsView';
 export { default as UILink } from './UILink';
 export { default as UILoadMoreButton } from './UILoadMoreButton';
-export { default as UINetworkStatus } from './UINetworkStatus';
 export { default as UIPureComponent } from './UIPureComponent';
 export { default as UIScaleButton } from './UIScaleButton';
 export { default as UISpinnerOverlay } from './UISpinnerOverlay';
