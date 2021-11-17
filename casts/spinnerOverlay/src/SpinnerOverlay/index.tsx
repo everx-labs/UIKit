e6b4ccdc60372aa5182b95ba7161966d3f813d9d
Marina <wearedusa@icloud.com>
2021-11-17 19:12:58 +0300

    Fix

--- casts/spinnerOverlay/src/SpinnerOverlay/index.tsx
+++ casts/spinnerOverlay/src/SpinnerOverlay/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Platform, StyleSheet, View, Modal } from 'react-native';
+import { Platform, StyleSheet, View, Modal, TextStyle } from 'react-native';
 import { WaveIndicator } from 'react-native-indicators';
+
+import { UIColor } from '@tonlabs/uikit.core';
+import { UILabel, UILabelColors, UILabelRoles } from '@tonlabs/uikit.themes';
 
 type Props = {
     visible: boolean;
+    titleContent: string;
+    textContent: string;
 };
 
 const waveConfig = {
@@ -11,16 +16,51 @@ const waveConfig = {
     count: 2,
 };
 
-export function UISpinnerOverlay({ visible = false }: Props) {
+export function UISpinnerOverlay({ visible = false, titleContent, textContent }: Props) {
+    const renderTitleContent = React.useMemo(() => {
+        const backgroundColor: TextStyle = {
+            backgroundColor: !titleContent ? 'transparent' : 'white',
+        };
+        return (
+            <UILabel
+                color={UILabelColors.TextPrimary}
+                role={UILabelRoles.ParagraphText}
+                style={[styles.titleContent, backgroundColor]}
+            >
+                {titleContent}
+            </UILabel>
+        );
+    }, [titleContent]);
+
+    const renderTextContent = React.useMemo(() => {
+        const backgroundColor: TextStyle = {
+            backgroundColor: !textContent ? 'transparent' : UIColor.primary(),
+        };
+
+        return (
+            <UILabel
+                color={UILabelColors.TextPrimary}
+                role={UILabelRoles.ParagraphText}
+                style={[styles.textContent, backgroundColor]}
+            >
+                {textContent}
+            </UILabel>
+        );
+    }, [textContent]);
+
     const renderSpinner = React.useMemo(() => {
         return (
             <View testID="spinner_loader" style={[styles.container]} key={`spinner~${Date.now()}`}>
                 <View style={styles.background}>
                     <WaveIndicator color="white" size={waveConfig.size} count={waveConfig.count} />
+                    <View style={styles.textContainer}>
+                        {renderTitleContent}
+                        {renderTextContent}
+                    </View>
                 </View>
             </View>
         );
-    }, []);
+    }, [renderTextContent, renderTitleContent]);
 
     if (!visible) {
         return null;
@@ -60,5 +100,34 @@ const styles = StyleSheet.create({
         right: 0,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    textContainer: {
+        flex: 1,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+    },
+    titleContent: {
+        marginBottom: 80,
+        textAlign: 'center',
+        color: 'red',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        overflow: 'hidden',
+    },
+    textContent: {
+        marginTop: 80,
+        textAlign: 'center',
+        color: 'white',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        overflow: 'hidden',
     },
 });
--- packages/legacy/src/index.js
+++ packages/legacy/src/index.js
@@ -2,4 +2,3 @@
 export { default as UIAccountPicker } from './UIAccountPicker';
 export { default as UIIdleDetector } from './UIIdleDetector';
 export { default as UIProfileInitials } from './UIProfileInitials';
-export { default as UISharedComponents } from './UISharedComponents';
