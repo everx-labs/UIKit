b5a40f6b01c794a27005a95527b0a8a624e4a942
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-11-18 17:28:05 +0300

    Add react-native-video. (#1356)
    * Add react-native-video.
    
    * Add react-native-video for android.
    
    * Add react-player package for web.
    
    * Add UIVideo.
    
    * Add react-native-video for android.
    
    * Add UIVideo.
    
    * Add UIVideo.
    
    * Update kit/media/src/UIVideo/hooks.ts
    
    Co-authored-by: Anton S <serkov_anton@mail.ru>
    
    * Add react-native-video.
    
    * Add tonlabs react-native-video.
    
    * Megre develop.
    
    Co-authored-by: Anton S <serkov_anton@mail.ru>

---
+++ kit/media/src/UIVideo/UIVideo.native.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import RNVideo from 'react-native-video';
+import type { UIVideoProps } from './types';
+
+export const UIVideo: React.FC<UIVideoProps> = ({
+    uri,
+    controls,
+    repeat,
+    width,
+    height,
+    aspectRatio,
+}: UIVideoProps) => {
+    return (
+        <RNVideo
+            source={{ uri }}
+            style={{
+                width,
+                height,
+                aspectRatio,
+            }}
+            controls={controls}
+            repeat={repeat}
+        />
+    );
+};
---
+++ kit/media/src/UIVideo/UIVideo.web.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import ReactPlayer from 'react-player';
+import { useDimensionsByAspectRatio } from './hooks';
+import type { UIVideoProps } from './types';
+
+export const UIVideo: React.FC<UIVideoProps> = ({
+    uri,
+    controls,
+    repeat,
+    width,
+    height,
+    aspectRatio,
+}: UIVideoProps) => {
+    const dimensions = useDimensionsByAspectRatio(width, height, aspectRatio);
+
+    return <ReactPlayer url={uri} controls={controls} {...dimensions} loop={repeat} />;
+};
