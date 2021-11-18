1f63eed7e621f748a8f88646668d98afabfd593e
sertony <serkov_anton@mail.ru>
2021-11-18 17:47:07 +0300

    fix spinner overlay import

--- packages/navigation_legacy/src/UIController/index.js
+++ packages/navigation_legacy/src/UIController/index.js
@@ -17,7 +17,8 @@ import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
 
 import { UIConstant, UIDevice, UIEventHelper, UIFunction, UIStyle } from '@tonlabs/uikit.core';
 import type { SafeAreaInsets } from '@tonlabs/uikit.core';
-import { UIAlertView, UIComponent, UISpinnerOverlay } from '@tonlabs/uikit.components';
+import { UIAlertView, UIComponent } from '@tonlabs/uikit.components';
+import { UISpinnerOverlay } from '@tonlabs/uicast.spinner-overlay';
 import { UISafeAreaView } from '@tonlabs/uikit.layout';
 import { UIBackgroundViewColors } from '@tonlabs/uikit.themes';
 import { uiLocalized } from '@tonlabs/localization';
