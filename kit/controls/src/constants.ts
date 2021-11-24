97a6280ed8d85f7b5db6dd8a93408d29e72cdcee
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-11-24 12:42:15 +0300

    Remove unnecessary commented code.

--- kit/controls/src/constants.ts
+++ kit/controls/src/constants.ts
@@ -13,45 +13,11 @@ export const UIConstant = {
     pillButtonHeight: 32,
     linkButtonHeight: 48,
 
-    // smallCellHeight: 24,
-    // defaultCellHeight: 48,
-
-    // minimumWidthToShowFoldingNotice: 400,
-    // minNoticeSize: 88,
-    // minNoticeIconSize: 64,
-    // maxNoticeIconSize: 88,
-
     tinyContentOffset: 4,
     smallContentOffset: 8,
     normalContentOffset: 12,
-    // contentOffset: 16,
-    // borderRadius: 8,
     alertBorderRadius: 12,
     pillButtonBorderRadius: 20,
-
-    // cardShadow:
-    //     Platform.OS === 'android'
-    //         ? {
-    //               elevation: 8,
-    //           }
-    //         : {
-    //               shadowColor: '#000000',
-    //               shadowOpacity: 0.2,
-    //               shadowRadius: 20,
-    //               shadowOffset: {
-    //                   width: 0,
-    //                   height: 4,
-    //               },
-    //           },
-
-    // dashSymbol: '\u2014',
-
-    // hitSlop: {
-    //     top: 24,
-    //     bottom: 24,
-    //     left: 24,
-    //     right: 24,
-    // },
 
     switcher: {
         circlePadding: 2,
