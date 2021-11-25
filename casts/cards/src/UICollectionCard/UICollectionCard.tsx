76140d0b25e6ee06f3bfa6d876fea22246737b6d
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-11-25 10:47:00 +0300

    Add CollectionCard.

--- casts/cards/src/UICollectionCard/UICollectionCard.tsx
+++ casts/cards/src/UICollectionCard/UICollectionCard.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { ColorVariants, makeStyles, Theme, useTheme } from '@tonlabs/uikit.themes';
 import { TouchableOpacity } from '@tonlabs/uikit.controls';
@@ -23,16 +23,18 @@ export function UICollectionCard({
     const styles = useStyles(theme);
     return (
         <UISkeleton show={!!loading} style={styles.skeleton}>
-            <TouchableOpacity testID={testID} onPress={onPress} style={styles.container}>
-                <Preview source={source} style={styles.preview} contentType={contentType} />
-                <LinearGradient
-                    start={{ x: 0, y: 0 }}
-                    end={{ x: 0, y: 1 }}
-                    colors={[UIConstant.linearGradientStart, UIConstant.linearGradientEnd]}
-                    style={styles.gradient}
-                />
-                <Title title={title} />
-                <Badge badge={badge} />
+            <TouchableOpacity testID={testID} onPress={onPress} style={styles.touchable}>
+                <View style={styles.container}>
+                    <Preview source={source} style={styles.preview} contentType={contentType} />
+                    <LinearGradient
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 0, y: 1 }}
+                        colors={[UIConstant.linearGradientStart, UIConstant.linearGradientEnd]}
+                        style={styles.gradient}
+                    />
+                    <Title title={title} />
+                    <Badge badge={badge} />
+                </View>
             </TouchableOpacity>
         </UISkeleton>
     );
@@ -40,26 +42,32 @@ export function UICollectionCard({
 
 const useStyles = makeStyles((theme: Theme) => ({
     skeleton: {
-        /**
-         * The radius of the skeleton has to be made a little smaller
-         * so that the corners of the content are not visible
-         */
-        borderRadius: UIConstant.uiCollectionCard.borderRadius - 2,
+        borderRadius: UIConstant.uiCollectionCard.borderRadius,
+    },
+    touchable: {
+        borderRadius: UIConstant.uiCollectionCard.borderRadius,
+        overflow: 'hidden',
     },
     container: {
-        borderRadius: UIConstant.uiCollectionCard.borderRadius,
         backgroundColor: theme[ColorVariants.BackgroundOverlay],
         padding: UILayoutConstant.contentOffset,
         justifyContent: 'space-between',
         aspectRatio: 1,
-        overflow: 'hidden',
     },
     preview: {
         ...StyleSheet.absoluteFillObject,
-        zIndex: -20,
+        ...Platform.select({
+            web: {
+                zIndex: -20,
+            },
+        }),
     },
     gradient: {
         ...StyleSheet.absoluteFillObject,
-        zIndex: -10,
+        ...Platform.select({
+            web: {
+                zIndex: -10,
+            },
+        }),
     },
 }));
