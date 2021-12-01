51a0fda088a3f1530eb5ddfd1bf98b02ac3bb8e8
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2021-12-01 17:28:47 +0300

    Added animation to the collection card.

--- casts/cards/src/UICollectionCard/types.ts
+++ casts/cards/src/UICollectionCard/types.ts
@@ -2,11 +2,29 @@ import type { ViewStyle, ImageSourcePropType } from 'react-native';
 import type { MediaCardContent } from '../types';
 
 export type UICollectionCardProps = {
+    /**
+     * Media content array
+     */
     contentList: MediaCardContent[];
+    /**
+     * Card title
+     */
     title?: string;
+    /**
+     * Displays the number of items in the collection or an image
+     */
     badge?: string | ImageSourcePropType;
+    /**
+     * Сallback called by clicking/tapping on the card
+     */
     onPress?: () => void;
+    /**
+     * Used to display the data loading process
+     */
     loading?: boolean;
+    /**
+     * ID for usage in tests
+     */
     testID?: string;
 };
 
