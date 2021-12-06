59d8fb63849302b5e6b82d4b46fd9a43f37e6f15
Aleksei Saveliev <savelichalex93@gmail.com>
2021-12-06 16:42:15 +0300

    Adjust padding a bit

--- casts/rows/src/UICurrencyRow.tsx
+++ casts/rows/src/UICurrencyRow.tsx
@@ -101,8 +101,7 @@ export function UICurrencyRow({
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        paddingTop: 15,
-        paddingBottom: UILayoutConstant.contentInsetVerticalX4,
+        paddingVertical: UILayoutConstant.contentInsetVerticalX4,
         alignItems: 'center',
     },
     inner: {
