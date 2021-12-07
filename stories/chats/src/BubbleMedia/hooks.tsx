0f5e84f8005c7355c00d4d25179498b45e6e8b55
vetkamak <vetka.mak@gmail.com>
2021-12-07 18:14:10 +0700

    add get image size failure handling

--- stories/chats/src/BubbleMedia/hooks.tsx
+++ stories/chats/src/BubbleMedia/hooks.tsx
@@ -166,19 +166,25 @@ export const useImageSize = (data: string | null, originalSize?: ImageSize) => {
      */
     React.useEffect(() => {
         if (data && !originalSize) {
-            Image.getSize(data, (width, height) => {
-                originalSizeRef.current = {
-                    width,
-                    height,
-                };
-                const newImageSize = getImageSize(
-                    originalSizeRef.current?.width,
-                    originalSizeRef.current?.height,
-                    maxImageSize.width,
-                    maxImageSize.height,
-                );
-                setImageSize(newImageSize);
-            });
+            Image.getSize(
+                data,
+                (width, height) => {
+                    originalSizeRef.current = {
+                        width,
+                        height,
+                    };
+                    const newImageSize = getImageSize(
+                        originalSizeRef.current?.width,
+                        originalSizeRef.current?.height,
+                        maxImageSize.width,
+                        maxImageSize.height,
+                    );
+                    setImageSize(newImageSize);
+                },
+                () => {
+                    setImageSize(maxImageSize);
+                },
+            );
         }
         /**
          * `maxImageSize` is not added to the list of dependencies,
