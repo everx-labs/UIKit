d1ca7f0326eac4b8da47fe8500a2889b4e7e53c5
Alexey Savelev <savelichalex93@gmail.com>
2021-12-16 11:39:58 +0300

    Implementation for iOS (#1400)

---
+++ kit/scrolls/ios/UIKitAccordionOverlayViewManager.h
@@ -0,0 +1,13 @@
+//
+//  UIKitAccordionOverlayViewManager.h
+//  UIKitScrolls
+//
+//  Created by Aleksei Savelev on 14/12/2021
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTBridgeModule.h>
+
+@interface UIKitAccordionOverlayViewManager : RCTViewManager
+@end
