fa1c7f89ae652eeeead5f38a0d544fbae25727f3
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2022-01-20 16:29:50 +0300

    Add UIForeground.

--- Example/src/screens/Foregrounds.tsx
+++ Example/src/screens/Foregrounds.tsx
@@ -48,55 +48,55 @@ const foregroundList = (
                 <UIForeground.ActionElement title="Action" />
             </UIForeground.PrimaryPart>
             <UIForeground.SecondaryPart>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} onPress={() => null} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} onPress={() => null} />
             </UIForeground.SecondaryPart>
         </UIForeground.Container>
 
         <UIForeground.Container>
             <UIForeground.PrimaryPart>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} />
                 <UIForeground.ActionElement title="Section" />
             </UIForeground.PrimaryPart>
             <UIForeground.SecondaryPart>
                 <UIForeground.TextElement>Text</UIForeground.TextElement>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} disabled />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} disabled />
             </UIForeground.SecondaryPart>
         </UIForeground.Container>
 
         <UIForeground.Container>
             <UIForeground.PrimaryPart>
                 <UIForeground.IconElement
-                    source={UIAssets.icons.ui.search}
+                    source={UIAssets.icons.ui.camera}
                     tintColor={ColorVariants.TextAccent}
                 />
                 <UIForeground.TextElement>Text</UIForeground.TextElement>
             </UIForeground.PrimaryPart>
             <UIForeground.SecondaryPart>
                 <UIForeground.ActionElement title="Action" onPress={() => null} />
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} onPress={() => null} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} onPress={() => null} />
             </UIForeground.SecondaryPart>
         </UIForeground.Container>
 
         <UIForeground.Container>
             <UIForeground.PrimaryPart onPress={() => null} negative>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} />
                 <UIForeground.ActionElement title="Negative" />
             </UIForeground.PrimaryPart>
             <UIForeground.SecondaryPart>
                 <UIForeground.NumberElement>{1234567890}</UIForeground.NumberElement>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} onPress={() => null} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} onPress={() => null} />
             </UIForeground.SecondaryPart>
         </UIForeground.Container>
 
         <UIForeground.Container>
             <UIForeground.PrimaryPart onPress={() => null}>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} />
                 <UIForeground.ActionElement title="Action" />
             </UIForeground.PrimaryPart>
             <UIForeground.SecondaryPart>
-                <UIForeground.IconElement source={UIAssets.icons.ui.search} onPress={() => null} />
+                <UIForeground.IconElement source={UIAssets.icons.ui.camera} onPress={() => null} />
                 <UIForeground.IconElement
-                    source={UIAssets.icons.ui.search}
+                    source={UIAssets.icons.ui.camera}
                     onPress={() => null}
                     tintColor={ColorVariants.TextAccent}
                 />
