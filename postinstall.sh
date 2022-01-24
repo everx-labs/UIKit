e5886aab54624fb3f09b0d37f485bfd44e318808
sertony <serkov_anton@mail.ru>
2022-01-24 16:53:19 +0300

    self review

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@
 sed -i '' -e '195s/.*/      } else if (typeof strings[key] !== "string" \&\& typeof strings[key].valueOf() !== "string") {/' ./node_modules/localized-strings/lib/LocalizedStrings.js
 
 # Patch Android
-## Remove `jcenter`` in favour of `google` or `mavenCentral``
+## Remove `jcenter` in favour of `google` or `mavenCentral`
 sed -i '' -e '3s/.*/    google()\
     mavenCentral()/' ./node_modules/react-native-android-keyboard-adjust/android/build.gradle
 sed -i '' -e '7s/.*/    classpath("com.android.tools.build:gradle:4.2.2")/' ./node_modules/react-native-android-keyboard-adjust/android/build.gradle
