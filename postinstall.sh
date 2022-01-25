7110e6ea1d85b6bfa86d3cbf1d162cc80b1d254a
sertony <serkov_anton@mail.ru>
2022-01-25 14:54:59 +0300

    fix patch path

--- postinstall.sh
+++ postinstall.sh
@@ -28,8 +28,8 @@ sed -i '' -e '7s/.*/    classpath("com.android.tools.build:gradle:4.2.2")/' ./no
 sed -i '' -e '7s/.*/    mavenCentral()/' ./node_modules/react-native-pager-view/android/build.gradle
 
 ### Patch `react-native-svg`
-sed -i '' -e '8s/.*/    mavenCentral()/' ./node_modules/react-native-pager-view/android/build.gradle
-sed -i '' -e '39s/.*/    mavenCentral()/' ./node_modules/react-native-pager-view/android/build.gradle
+sed -i '' -e '8s/.*/    mavenCentral()/' ./node_modules/react-native-svg/android/build.gradle
+sed -i '' -e '39s/.*/    mavenCentral()/' ./node_modules/react-native-svg/android/build.gradle
 
 ## Jetify Android dependencies
 ./node_modules/.bin/jetify
