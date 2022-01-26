ad9c826db0e14beef92e71d8d39a423cc86da795
Roman Tishakov <romantishakov@gmail.com>
2022-01-26 18:38:36 +0300

    Key sheet input update (#1476)
    * Add button to UIKeySheet
    
    * Fix typings.
    
    * Review fixes.
    
    * Review fixes.
    
    * Update localization/src/languages/ko.json
    
    Co-authored-by: Anton S <serkov_anton@mail.ru>
    
    * Update localization/src/languages/ko.json
    
    Co-authored-by: Anton S <serkov_anton@mail.ru>
    
    Co-authored-by: Anton S <serkov_anton@mail.ru>

--- stories/browser/src/UIKeySheet.tsx
+++ stories/browser/src/UIKeySheet.tsx
@@ -2,19 +2,35 @@ import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import { UIKeyTextView, UIMaterialTextViewRef } from '@tonlabs/uikit.inputs';
+import { UIBoxButton } from '@tonlabs/uikit.controls';
 
 import { UIPullerSheet } from './UIPullerSheet';
+
+function useKeySheetContent() {
+    const [error, setError] = React.useState(false);
+    const [success, setSuccess] = React.useState(false);
+
+    const key = React.useRef('');
+    const setKey = React.useCallback((val: string) => {
+        key.current = val;
+    }, []);
+
+    return { error, setError, success, setSuccess, key, setKey };
+}
 
 // We wrap it with a component to be able
 // to focus input on every new sheet mounting
 function UIKeySheetContent({
     onKeyRetrieved,
     label,
+    buttonTitle,
 }: {
     onKeyRetrieved: (key: string) => void;
     label: string;
+    buttonTitle: string;
 }) {
     const keyRef = React.useRef<UIMaterialTextViewRef>(null);
+    const { success, setSuccess, error, setError, key, setKey } = useKeySheetContent();
 
     React.useLayoutEffect(() => {
         function focus() {
@@ -30,9 +46,25 @@ function UIKeySheetContent({
         setTimeout(focus, 300);
     }, []);
 
+    const onSend = React.useCallback(() => {
+        if (success) {
+            onKeyRetrieved(key.current);
+        }
+    }, [key, success]);
+
+    const disabled = React.useMemo(() => !success || error, [success, error])
+
     return (
         <View style={styles.wrapper}>
-            <UIKeyTextView ref={keyRef} label={label} onDone={onKeyRetrieved} />
+            <UIKeyTextView
+                ref={keyRef}
+                label={label}
+                onDone={onKeyRetrieved}
+                onSuccess={setSuccess}
+                onError={setError}
+                onChangeText={setKey}
+            />
+            <UIBoxButton onPress={onSend} disabled={disabled} title={buttonTitle} />
         </View>
     );
 }
@@ -42,15 +74,21 @@ export function UIKeySheet({
     onClose,
     onKeyRetrieved,
     label,
+    buttonTitle,
 }: {
     visible: boolean;
     onClose: () => void;
     onKeyRetrieved: (key: string) => void;
     label: string;
+    buttonTitle: string;
 }) {
     return (
         <UIPullerSheet visible={visible} onClose={onClose}>
-            <UIKeySheetContent onKeyRetrieved={onKeyRetrieved} label={label} />
+            <UIKeySheetContent
+                onKeyRetrieved={onKeyRetrieved}
+                label={label}
+                buttonTitle={buttonTitle}
+            />
         </UIPullerSheet>
     );
 }
