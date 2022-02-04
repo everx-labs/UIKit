aeaf9f4cf8285c8b260422863ef91d8fc65b41f2
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2022-02-04 11:20:45 +0300

    Update UIBottomSheet.

--- stories/browser/src/UIPullerSheet.tsx
+++ stories/browser/src/UIPullerSheet.tsx
@@ -1,25 +1,18 @@
 import * as React from 'react';
-import { useWindowDimensions, StatusBar } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { UIConstant } from '@tonlabs/uikit.core';
 import { UIBottomSheet, useIntrinsicSizeScrollView } from '@tonlabs/uikit.popups';
 import { ScrollView } from '@tonlabs/uikit.scrolls';
 
-const PULLER_HEIGHT = 12;
-
 function SheetContent({ children }: { children: React.ReactNode }) {
     const insets = useSafeAreaInsets();
-    const { height } = useWindowDimensions();
 
     const paddingBottom = Math.max(insets?.bottom || 0, UIConstant.contentOffset());
     const { style: scrollIntrinsicStyle, onContentSizeChange } = useIntrinsicSizeScrollView();
     return (
         <ScrollView
             bounces={false}
-            style={{
-                height: height - (StatusBar.currentHeight ?? 0) - insets.top - PULLER_HEIGHT,
-            }}
             contentContainerStyle={{
                 paddingBottom,
             }}
