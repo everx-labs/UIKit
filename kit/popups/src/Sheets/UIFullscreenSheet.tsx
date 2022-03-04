cb03a60160d962657fb826896c0602982547e751
Alexey Savelev <savelichalex93@gmail.com>
2022-03-04 17:34:02 +0800

    Fix UIFullscreenSheet web height (#1525)
    * Fix UIFullscreenSheet web height
    
    * Add comments

--- kit/popups/src/Sheets/UIFullscreenSheet.tsx
+++ kit/popups/src/Sheets/UIFullscreenSheet.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import { Platform, StyleProp, StyleSheet, ViewStyle } from 'react-native';
 
 import { UILayoutConstant } from '@tonlabs/uikit.layout';
 import { useDimensions } from '@tonlabs/uikit.inputs';
+
 import { UISheet, UISheetProps } from './UISheet/UISheet';
 
 export type UIFullscreenSheetProps = UISheetProps & {
@@ -11,13 +12,30 @@ export type UIFullscreenSheetProps = UISheetProps & {
 
 export function UIFullscreenSheet({ children, style, ...rest }: UIFullscreenSheetProps) {
     const {
-        screen: { height },
+        screen: { height: screenHeight },
+        window: { height: windowHeight },
     } = useDimensions();
 
-    const fullscreenHeight = React.useMemo(
-        () => height + UILayoutConstant.rubberBandEffectDistance,
-        [height],
-    );
+    const fullscreenHeight = React.useMemo(() => {
+        /**
+         * On different platforms it behave differently.
+         *
+         * On web `screenHeight` is equal to a device screen height
+         * a browser window is usually smaller (and can be resized).
+         */
+        if (Platform.OS === 'web') {
+            return windowHeight + UILayoutConstant.rubberBandEffectDistance;
+        }
+
+        /**
+         * On iOS it seems `windowHeight` is equal to screenHeight.
+         *
+         * On Android `windowHeight` doesn't include a status bar height and
+         * a navigation bar (the one on the bottom). Since we use edge-to-edge
+         * right now we want to use `screenHeight` to be fullscreen.
+         */
+        return screenHeight + UILayoutConstant.rubberBandEffectDistance;
+    }, [screenHeight, windowHeight]);
 
     const sheetStyle = React.useMemo(() => {
         const flattenStyle = StyleSheet.flatten(style);
