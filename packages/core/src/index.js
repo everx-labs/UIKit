2eb436d064b390c2e5046a014a301226bc3198e9
sertony <serkov_anton@mail.ru>
2022-03-14 18:59:37 +0300

    remove UIEventHelper

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -5,7 +5,6 @@ export { default as UIColor } from './UIColor';
 export { default as UIColorPalette } from './UIColor/UIColorPalette';
 export { default as UIConstant } from './UIConstant';
 export { default as UIDevice } from './UIDevice';
-export { default as UIEventHelper } from './UIEventHelper';
 export { default as UIFunction } from './UIFunction';
 export { default as UIStyle } from './UIStyle';
 export { default as UIStyleColor } from './UIStyle/UIStyleColor';
