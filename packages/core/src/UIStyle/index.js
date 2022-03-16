c42b7144c3ed69e66e301476a5ccb1440e027798
vetkamak <vetka.mak@gmail.com>
2022-03-17 00:25:30 +0700

    remove unused navigation styles

--- packages/core/src/UIStyle/index.js
+++ packages/core/src/UIStyle/index.js
@@ -1,8 +1,6 @@
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import UIColor from '../UIColor';
 import UIConstant from '../UIConstant';
-import UIDevice from '../UIDevice';
 import UIStyleMargin, { marginStyles } from './UIStyleMargin';
 import UIStylePadding, { paddingStyles } from './UIStylePadding';
 import UIStyleCommon, { commonStyles } from './UIStyleCommon';
@@ -16,39 +14,6 @@ const UIStyle = StyleSheet.create({
     ...flexStyles,
     ...marginStyles,
     ...paddingStyles,
-
-    // navigator
-    navigatorHeader: {
-        backgroundColor: UIColor.backgroundPrimary(),
-        overflow: 'hidden',
-        borderWidth: 0,
-        height: UIDevice.navigationBarHeight(),
-        borderBottomColor: 'transparent',
-        elevation: Platform.select({
-            android: 0,
-        }),
-    },
-    reactNavigationHeader: {
-        backgroundColor: UIColor.backgroundPrimary(),
-        borderWidth: 0,
-        height: UIDevice.navigationBarHeight(),
-        borderBottomColor: 'transparent',
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        elevation: Platform.select({
-            android: 0,
-        }),
-    },
-    navigatorButton: {
-        marginHorizontal: UIConstant.normalContentOffset(),
-        height: 32,
-        width: 32,
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: 'hidden',
-    },
 
     // split view controller
     splitViewController: {
