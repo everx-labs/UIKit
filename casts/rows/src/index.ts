9ebfdebb165d4cb068762ce3fde633dbe00f1af2
lochmatij <romanov.ds35@gmail.com>
2022-03-31 17:51:34 +0300

    add export from rows

--- casts/rows/src/index.ts
+++ casts/rows/src/index.ts
@@ -3,5 +3,5 @@ export * from './UICurrencyRow';
 export * from './UILink';
 
 export * from './UIListRowsAdapter';
-export { UIRowsPressabilityProvider } from './UIListRowsContext';
+export { UIRowsPressabilityProvider, useUIRowsPressability } from './UIListRowsContext';
 export * from './types';
