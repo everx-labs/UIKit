50475357cc0b1e3334fe9eb6a172585e837c40bc
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2022-04-07 16:41:40 +0300

    Change keyboard type for amount input.

--- kit/inputs/src/UIAmountInput/UIAmountInput.tsx
+++ kit/inputs/src/UIAmountInput/UIAmountInput.tsx
@@ -37,7 +37,7 @@ export const UIAmountInput = React.forwardRef<UIAmountInputRef, UIAmountInputPro
                 success={success}
                 mask={mask}
                 onChangeText={onChangeText}
-                keyboardType="decimal-pad"
+                keyboardType="numeric"
             />
         );
     },
