981f714b722ac43055d5994b105091e021cc21f4
Alexey Savelev <savelichalex93@gmail.com>
2022-04-15 21:51:22 +0800

    Drop UISafeAreaView (#1578)

--- kit/layout/src/index.ts
+++ kit/layout/src/index.ts
@@ -1,18 +1,15 @@
 import * as Portal from './Portal';
-import * as UISafeAreaView from './UISafeAreaView';
 import { UILayoutConstant } from './UILayoutConstant';
 import { UISkeleton } from './Skeleton';
 import { AnimationHelpers } from './AnimationHelpers';
 
 export * from './Portal';
-export * from './UISafeAreaView';
 export * from './UILayoutConstant';
 export * from './Skeleton';
 export * from './AnimationHelpers';
 
 export const UILayout = {
     Portal,
-    UISafeAreaView,
     UISkeleton,
 
     UILayoutConstant,
