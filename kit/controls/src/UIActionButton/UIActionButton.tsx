45ead2fe74e5ced62ad7d0d929bd7c4a590f750f
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2022-05-11 15:42:25 +0300

    Transfer UIActionButton to Pressable component. (#1593)

--- kit/controls/src/UIActionButton/UIActionButton.tsx
+++ kit/controls/src/UIActionButton/UIActionButton.tsx
@@ -1,155 +1,25 @@
 import * as React from 'react';
-import { ImageSourcePropType, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import type { View } from 'react-native';
 
-import { TypographyVariants, makeStyles } from '@tonlabs/uikit.themes';
+import { Pressable } from '../Pressable';
+import type { UIActionButtonProps } from './types';
+import { ActionButtonContent } from './ActionButtonContent';
 
-import { UIConstant } from '../constants';
-import { ActionButtonIcon } from './ActionButtonIcon';
-import { ButtonTitle } from '../Button/useButtonChildren';
-import { TouchableElement } from '../Button/TouchableElement';
-import { ActionButtonAnimations, ContentAnimations, UIActionButtonType } from './types';
-import {
-    useButtonAnimations,
-    useButtonColorScheme,
-    useButtonColors,
-    useContentAnimatedStyles,
-} from './hooks';
-import type { UILayout } from '../types';
+export const UIActionButton = React.forwardRef<View, UIActionButtonProps>(function UIActionButton(
+    props: UIActionButtonProps,
+    ref,
+) {
+    const { disabled, loading, onPress, testID } = props;
 
-export type UIActionButtonProps = {
-    /**
-     * Whether the button is disabled or not; if true a button is grayed out and `onPress` does no response
-     */
-    disabled?: boolean;
-    /**
-     * Source for the button icon
-     */
-    icon?: ImageSourcePropType;
-    /**
-     * Allows to set top, right, bottom and left margins to the button container
-     */
-    layout?: UILayout;
-    /**
-     * Whether to display a loading indicator instead of button content or not
-     */
-    loading?: boolean;
-    /**
-     * Function will be called on button press
-     */
-    onPress?: () => void | Promise<void>;
-    /**
-     * ID for usage in tests
-     */
-    testID?: string;
-    /**
-     * Text displayed on the button
-     */
-    title?: string;
-    /**
-     * Type of the button; specific type allows to set the corresponding accent
-     * - `UIActionButtonType.Primary` - button with current theme background BW color (default)
-     * - `UIActionButtonType.Accent` - button with current theme background accent color
-     */
-    type?: UIActionButtonType;
-};
-
-export const UIActionButton = React.forwardRef<TouchableWithoutFeedback, UIActionButtonProps>(
-    function UIActionButton(
-        {
-            disabled,
-            icon,
-            layout,
-            loading,
-            onPress,
-            testID,
-            title,
-            type = UIActionButtonType.Primary,
-        }: UIActionButtonProps,
-        ref,
-    ) {
-        const { overlay, content } = useButtonColorScheme(type);
-        const { contentColor, backgroundColor } = useButtonColors(disabled, overlay, content);
-
-        const buttonAnimations: ActionButtonAnimations = useButtonAnimations(
-            backgroundColor,
-            overlay.hover,
-            overlay.pressed,
-        );
-
-        const contentAnimations: ContentAnimations = useContentAnimatedStyles(
-            buttonAnimations.hover.animationParam,
-            buttonAnimations.press.animationParam,
-            content.hover,
-            content.pressed,
-            contentColor,
-        );
-
-        const styles = useStyles(icon, loading);
-
-        const handleOnPress = React.useCallback(() => {
-            if (!loading && onPress != null) {
-                onPress();
-            }
-        }, [loading, onPress]);
-
-        return (
-            <TouchableElement
-                ref={ref}
-                onPress={handleOnPress}
-                animations={buttonAnimations}
-                disabled={disabled || loading}
-                loading={loading}
-                style={[styles.container, layout]}
-                testID={testID}
-            >
-                <View style={styles.content}>
-                    <View style={styles.actionButtonIcon}>
-                        <ActionButtonIcon
-                            icon={icon}
-                            loading={loading}
-                            animatedProps={contentAnimations.iconProps}
-                            initialColor={contentColor}
-                        />
-                    </View>
-                    {title != null ? (
-                        <ButtonTitle
-                            style={styles.title}
-                            titleRole={TypographyVariants.NarrowActionNote}
-                            titleColor={contentColor}
-                            titleAnimStyle={contentAnimations.titleStyle}
-                        >
-                            {title}
-                        </ButtonTitle>
-                    ) : null}
-                </View>
-            </TouchableElement>
-        );
-    },
-);
-
-const useStyles = makeStyles((icon: ImageSourcePropType | undefined, loading: boolean) => ({
-    container: {
-        height: UIConstant.actionButtonHeight,
-        minWidth: UIConstant.actionButtonHeight,
-        borderRadius: UIConstant.alertBorderRadius,
-        overflow: 'hidden',
-    },
-    content: {
-        paddingHorizontal: UIConstant.normalContentOffset,
-        flexDirection: 'row',
-    },
-    leftIcon: {
-        marginRight: UIConstant.smallContentOffset,
-        height: UIConstant.actionButtonIconSize,
-        width: UIConstant.actionButtonIconSize,
-    },
-    title: {
-        paddingLeft: icon ? UIConstant.smallContentOffset : 0,
-        opacity: loading && !icon ? 0 : 1,
-    },
-    actionButtonIcon: {
-        ...(icon ? null : StyleSheet.absoluteFillObject),
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-}));
+    return (
+        <Pressable
+            ref={ref}
+            testID={testID}
+            disabled={disabled}
+            loading={loading}
+            onPress={onPress}
+        >
+            <ActionButtonContent {...props} />
+        </Pressable>
+    );
+});
--- kit/controls/src/UIActionButton/types.ts
+++ kit/controls/src/UIActionButton/types.ts
@@ -1,24 +1,58 @@
 import type { ColorVariants } from '@tonlabs/uikit.themes';
 import type { ColorValue, ImageSourcePropType, TextStyle } from 'react-native';
+import type Animated from 'react-native-reanimated';
 import type { AnimatedStyleProp } from 'react-native-reanimated';
 import type { BackgroundParams } from '../Button/types';
+import type { UILayout } from '../types';
+import type { UIActionButtonType } from './constants';
+
+export type UIActionButtonProps = {
+    /**
+     * Whether the button is disabled or not; if true a button is grayed out and `onPress` does no response
+     */
+    disabled?: boolean;
+    /**
+     * Source for the button icon
+     */
+    icon?: ImageSourcePropType;
+    /**
+     * Allows to set top, right, bottom and left margins to the button container
+     */
+    layout?: UILayout;
+    /**
+     * Whether to display a loading indicator instead of button content or not
+     */
+    loading?: boolean;
+    /**
+     * Function will be called on button press
+     */
+    onPress?: () => void | Promise<void>;
+    /**
+     * ID for usage in tests
+     */
+    testID?: string;
+    /**
+     * Text displayed on the button
+     */
+    title?: string;
+    /**
+     * Type of the button; specific type allows to set the corresponding accent
+     * - `UIActionButtonType.Primary` - button with current theme background BW color (default)
+     * - `UIActionButtonType.Accent` - button with current theme background accent color
+     */
+    type?: UIActionButtonType;
+};
 
 export type ActionButtonAnimations = {
     hover: BackgroundParams;
     press: BackgroundParams;
 };
 
-// eslint-disable-next-line no-shadow
-export enum UIActionButtonType {
-    Primary = 'Primary',
-    Accent = 'Accent',
-}
-
-export type UIActionButtonIconProps = {
+export type ActionButtonIconProps = {
     icon?: ImageSourcePropType;
     loading?: boolean;
-    animatedProps: { tintColor?: ColorValue };
-    initialColor?: ColorVariants;
+    color: Animated.SharedValue<string | number>;
+    indicatorColor: ColorVariants;
 };
 
 export type ContentAnimations = {
