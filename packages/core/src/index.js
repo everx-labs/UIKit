a9ff484f924228485385a485115540b930b97ca1
sertony <serkov_anton@mail.ru>
2022-05-13 17:32:49 +0300

    remove UIFunction

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -3,11 +3,9 @@ import './configs';
 
 export { default as UIConstant } from './UIConstant';
 export { default as UIDevice } from './UIDevice';
-export { default as UIFunction } from './UIFunction';
 export { default as UIStyle } from './UIStyle';
 
 export type * from './UIColor/UIColorTypes';
 export type * from './UIDevice';
-export type * from './UIFunction';
 export type * from './types';
 export type * from './types/BigNum';
