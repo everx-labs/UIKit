cb910d202c273a3c36d109a0d36b33703b5af71d
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2022-05-24 18:07:28 +0300

    Try new input state management concept.

--- kit/inputs/src/UIMaterialTextView/UIMaterialTextView.tsx
+++ kit/inputs/src/UIMaterialTextView/UIMaterialTextView.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { SharedValue, useAnimatedReaction } from 'react-native-reanimated';
 import {
     getEmptyUIMaterialTextViewRef,
     MaterialTextViewIcon,
@@ -66,6 +67,104 @@ const UIMaterialTextViewForward = React.forwardRef<UIMaterialTextViewRef, UIMate
         );
     },
 );
+
+function useTextViewRef(passedRef: React.ForwardedRef<MaterialTextViewRef1>) {
+    const ref = React.useRef<MaterialTextViewRef1>(null);
+
+    React.useImperativeHandle<Record<string, any>, MaterialTextViewRef>(
+        passedRef,
+        (): MaterialTextViewRef => ({
+            ...emptyUIMaterialTextViewRef,
+            ...ref.current,
+        }),
+    );
+
+    return ref;
+}
+
+type TextViewStateParameters = {
+    isHoveredAnimated: Readonly<SharedValue<boolean>>;
+    isFocusedAnimated: Readonly<SharedValue<boolean>>;
+    hasValueAnimated: Readonly<SharedValue<boolean>>;
+};
+
+type MaterialTextViewRef1 = MaterialTextViewRef & TextViewStateParameters;
+
+type TextViewParameterName = 'isHovered' | 'isFocused' | 'hasValue';
+
+type TextViewParameters<T extends TextViewParameterName> = Record<T, true | undefined>;
+
+type TextViewState<T extends TextViewParameterName> = Record<T, Readonly<boolean | undefined>>;
+
+function useGetJSValue(animatedValue: Readonly<SharedValue<boolean>> | undefined): boolean {
+    if (!animatedValue) {
+        return false;
+    }
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [value, setValue] = React.useState<boolean>(animatedValue?.value || false);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useAnimatedReaction(
+        () => animatedValue?.value,
+        (currentValue: boolean | undefined, previousValue: boolean | undefined | null) => {
+            if (currentValue != null && currentValue !== previousValue) {
+                setValue(currentValue);
+            }
+        },
+    );
+    return value;
+}
+
+function useTextViewReactState<T extends TextViewParameterName>(
+    ref: React.RefObject<MaterialTextViewRef1>,
+    config: TextViewParameters<T>,
+): TextViewState<T> {
+    const configRef = React.useRef(config);
+
+    const result: TextViewState<T> = {} as TextViewState<T>;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in configRef.current) {
+        if (Object.prototype.hasOwnProperty.call(configRef.current, key)) {
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            result[key] = useGetJSValue(ref.current?.[`${key}Animated`]);
+        }
+    }
+    return result;
+}
+
+const UINewMaterialTextViewForward = React.forwardRef<
+    MaterialTextViewRef1,
+    UIMaterialTextViewProps
+>(function UINewMaterialTextViewForward(props: UIMaterialTextViewProps, passedRef) {
+    /**
+     * We can provide nothing to the useTextViewRef function. We provide passedRef if only when we have it.
+     *
+     * `ref` must be passed to any "Input" component (MaterialTextView, UIMaterialTextView, etc.)
+     * That "Input" component must have `isHoveredAnimated`, `isFocusedAnimated`, `hasValueAnimated`
+     * fields in their `ref`
+     *
+     */
+    const ref = useTextViewRef(passedRef);
+
+    const { isHoveredAnimated, isFocusedAnimated, hasValueAnimated } = ref.current ?? {};
+
+    const { isHovered, isFocused, hasValue } = useTextViewReactState(ref, {
+        isHovered: true,
+        isFocused: true,
+        hasValue: true,
+    });
+
+    isHovered;
+    isHoveredAnimated;
+    isFocused;
+    isFocusedAnimated;
+    hasValue;
+    hasValueAnimated;
+
+    return <MaterialTextView {...props} ref={ref} />;
+});
+
+UINewMaterialTextViewForward;
 
 // @ts-expect-error
 // ts doesn't understand that we assign [Icon|Action|Text] later, and want to see it right away
