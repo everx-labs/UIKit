4d8ab87167e87703fe584e489fd2229914c070f7
Aleksei Saveliev <savelichalex93@gmail.com>
2022-06-30 18:17:22 +0800

    Even better CADisplayLink usage optimization

--- casts/keyboard/ios/UIKitKeyboardIosFrameListener.m
+++ casts/keyboard/ios/UIKitKeyboardIosFrameListener.m
@@ -19,22 +19,16 @@
     CADisplayLink *displayLink;
     CGFloat prevKeyboardTopPosition;
     int _windowsCount;
-    CGFloat _lastKnownKeyboardY;
-    
-    BOOL displayLinkPausedByDefault;
 }
 - (instancetype)init
 {
     self = [super init];
     if (self) {
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowDidBecomeVisible:) name:UIWindowDidBecomeVisibleNotification object:nil];
         _listeners = [NSMapTable strongToStrongObjectsMapTable];
         prevKeyboardTopPosition = 0;
         _windowsCount = 0;
-        _lastKnownKeyboardY = 0;
-        
-        displayLinkPausedByDefault = YES;
     }
     return self;
 }
@@ -43,6 +37,103 @@ RCT_EXPORT_MODULE()
 
 + (BOOL)requiresMainQueueSetup {
     return YES;
+}
+
+#pragma mark Coords gathering
+
+/**
+ * At first there was an infinit CADisplayLink pulling,
+ * it started when listener was added, and stopped when last listener was gone.
+ * It wasn't perfect solution, since in a lot of situations keyboard wasn't actually
+ * shown, hence pulling did nothing, it was just a waste of CPU resource
+ * (constant load around 2%).
+ *
+ * To prevent that we started to listen to keyboard `show/hide` events
+ * and run CADisplayLink only when it's needed. Though it also didn't work well,
+ * since `UIKeyboardWillShowNotification` can fire even when actual soft keyboard aren't present,
+ * like when there is an `inputAccessoryView` and also when keyboard is open it still
+ * waste resources, just like it did before.
+ *
+ * To solve that, we use combination of a subscription for `windows` changes, plus
+ * KVO on `keyboardView` to see when animation is started, and run CADisplayLink
+ * only when it's actually needed.
+ * (See comment in `addFrameListener` on why KVO alone isn't enough).
+ */
+- (void)windowDidBecomeVisible:(NSNotification*)notification {
+    // No need to listen for frame changes if no listeners attached
+    if (displayLink == nil) {
+        return;
+    }
+    
+    UIView *keyboardView = [self findKeyboardView];
+    if (keyboardView == _keyboardView) {
+        return;
+    }
+    
+    _keyboardView = keyboardView;
+    
+    [keyboardView addObserver:self forKeyPath:@"center" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"center"] && object == _keyboardView) {
+        CGFloat layerFrameY = _keyboardView.layer.frame.origin.y;
+        CGFloat presentationLayerFrameY = [_keyboardView.layer presentationLayer].frame.origin.y;
+        
+        // start pulling if coords doesn't match
+        // as it shows that there's animation in progress
+        if (layerFrameY != presentationLayerFrameY) {
+            displayLink.paused = NO;
+        }
+        
+        [self callListenersIfNeeded:presentationLayerFrameY];
+    }
+}
+
+- (void)callListenersIfNeeded:(CGFloat)keyboardFrameY {
+    /**
+     * Starting from some new iOS version
+     * we found it on 15.5, it can return 0
+     * that is just wrong value, we don't need to react to
+     */
+    if (keyboardFrameY == 0) {
+        return;
+    }
+
+    CGFloat keyboardWindowH = self.keyboardView.window.bounds.size.height;
+    CGFloat keyboardTopPosition =
+        keyboardWindowH -
+        keyboardFrameY;
+    
+    if (keyboardTopPosition == prevKeyboardTopPosition) {
+        return;
+    }
+    
+    prevKeyboardTopPosition = keyboardTopPosition;
+    
+    for (NSNumber *key in _listeners) {
+        KeyboardFrameListener listener = [_listeners objectForKey:key];
+        if (listener != nil) {
+            listener(keyboardTopPosition);
+        }
+    }
+}
+
+- (void)pullKeyboardPosition {
+    if (self.keyboardView == nil) {
+        return;
+    }
+    
+    CGFloat layerFrameY = self.keyboardView.layer.frame.origin.y;
+    CGFloat presentationLayerFrameY = [self.keyboardView.layer presentationLayer].frame.origin.y;
+    
+    if (layerFrameY == presentationLayerFrameY) {
+        // We reached the end of the animationn
+        // no need to pull position anymore
+        displayLink.paused = YES;
+    }
+    
+    [self callListenersIfNeeded:presentationLayerFrameY];
 }
 
 /**
@@ -93,66 +184,14 @@ RCT_EXPORT_MODULE()
     return _keyboardView;
 }
 
-- (void)updateKeyboardFrameForListeners {
-    if (self.keyboardView == nil) {
-        return;
-    }
-    
-    CGFloat keyboardFrameY = [self.keyboardView.layer presentationLayer].frame.origin.y;
-    // Doing as little work as possible, as it runs a lot of times with CADisplayLink
-    if (keyboardFrameY == _lastKnownKeyboardY) {
-        return;
-    }
-
-    /**
-     * Starting from some new iOS version
-     * we found it on 15.5, it can return 0
-     * that is just wrong value, we don't need to react to
-     */
-    if (keyboardFrameY == 0) {
-        return;
-    }
-
-    CGFloat keyboardWindowH = self.keyboardView.window.bounds.size.height;
-    CGFloat keyboardTopPosition =
-        keyboardWindowH -
-        keyboardFrameY;
-    
-    if (keyboardTopPosition == prevKeyboardTopPosition) {
-        return;
-    }
-    
-    prevKeyboardTopPosition = keyboardTopPosition;
-    
-    for (NSNumber *key in _listeners) {
-        KeyboardFrameListener listener = [_listeners objectForKey:key];
-        if (listener != nil) {
-            listener(keyboardTopPosition);
-        }
-    }
-}
-
-- (void)keyboardWillShow:(NSNotification *)notification {
-    displayLinkPausedByDefault = NO;
-    
-    if (displayLink == nil) {
-        return;
-    }
-    displayLink.paused = NO;
-}
-
 - (void)keyboardDidHide:(NSNotification *)notification {
     /**
      * Invalidate it just in case
      */
     _keyboardView = nil;
-    displayLinkPausedByDefault = YES;
-    
-    if (displayLink == nil) {
-        return;
-    }
-    displayLink.paused = YES;
 }
+
+#pragma mark Listener management
 
 - (void)addFrameListener:(NSNumber *)uid withListener:(void (^)(CGFloat height))withListener {
     if (_listeners.count == 0) {
@@ -160,15 +199,15 @@ RCT_EXPORT_MODULE()
          * KVO host view's frame sometimes can't get updated values. So instead, we use display link pulling.
          * Source:
          * https://github.com/JunyuKuang/KeyboardUtility/blob/master/KeyboardUtility/KeyboardFrameObserversManager.swift#L60-L62
+         *
+         * UPD: See `windowDidBecomeVisible` for updated algorithm
          */
-        displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateKeyboardFrameForListeners)];
+        displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(pullKeyboardPosition)];
         [displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
         /**
-         * There're 2 situations:
-         * - when keyboard isn't opened yet, we should keep it paused to not waste CPU resources;
-         * - when keyboard is open during displayLink creation, let it work from the start.
+         * Keep it paused by default to not waste CPU resources
          */
-        displayLink.paused = displayLinkPausedByDefault;
+        displayLink.paused = YES;
     }
     [_listeners setObject:withListener forKey:uid];
 }
