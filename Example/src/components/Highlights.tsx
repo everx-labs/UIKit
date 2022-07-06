e0790f1061793c7b0cf0a57d475827005d2ddf37
Aleksei Saveliev <savelichalex93@gmail.com>
2022-07-06 14:12:03 +0800

    Add dots

--- Example/src/components/Highlights.tsx
+++ Example/src/components/Highlights.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Animated, {
+    Extrapolate,
+    interpolate,
     runOnJS,
+    scrollTo,
     useAnimatedReaction,
+    useAnimatedRef,
     useAnimatedScrollHandler,
+    useAnimatedStyle,
+    useDerivedValue,
     useSharedValue,
 } from 'react-native-reanimated';
 import { debounce } from 'lodash';
@@ -19,10 +25,10 @@ function binaryIndexSearch(positions: number[], x: number) {
         const item = positions[middle];
 
         if (x === item) {
-            return left;
+            return middle;
         }
 
-        if (x > item) {
+        if (x >= item) {
             left = middle;
         } else {
             right = middle;
@@ -89,8 +95,59 @@ function usePositions(itemsCount: number) {
             return 0;
         }
 
-        return binaryIndexSearch(sharedContext.value.positions, x);
+        const position = binaryIndexSearch(sharedContext.value.positions, x);
+        const leftX = sharedContext.value.positions[position];
+        const rightX = sharedContext.value.positions[position + 1];
+        const progress = (x - leftX) / (rightX - leftX);
+        return position + progress;
     }, []);
+
+    const calculateClosestPosition = React.useCallback(function calcClosestPosition(x: number) {
+        'worklet';
+
+        if (!sharedContext.value.isStable) {
+            return 0;
+        }
+
+        const closesLeftPosition = binaryIndexSearch(sharedContext.value.positions, x);
+        const leftX = sharedContext.value.positions[closesLeftPosition];
+        const rightX = sharedContext.value.positions[closesLeftPosition + 1];
+        const middleX = leftX + Math.trunc((rightX - leftX) / 2);
+
+        if (x < middleX) {
+            return leftX;
+        }
+        return rightX;
+    }, []);
+
+    const calculateClosestLeftPosition = React.useCallback(function calcClosestPosition(x: number) {
+        'worklet';
+
+        if (!sharedContext.value.isStable) {
+            return 0;
+        }
+
+        const closesLeftPosition = binaryIndexSearch(sharedContext.value.positions, x);
+        const leftX = sharedContext.value.positions[closesLeftPosition];
+
+        return leftX;
+    }, []);
+
+    const calculateClosestRightPosition = React.useCallback(function calcClosestPosition(
+        x: number,
+    ) {
+        'worklet';
+
+        if (!sharedContext.value.isStable) {
+            return 0;
+        }
+
+        const closesLeftPosition = binaryIndexSearch(sharedContext.value.positions, x);
+        const rightX = sharedContext.value.positions[closesLeftPosition + 1];
+
+        return rightX;
+    },
+    []);
 
     const onItemLayout = React.useCallback((index: number, value: number) => {
         'worklet';
@@ -104,47 +161,231 @@ function usePositions(itemsCount: number) {
 
     return {
         calculateCurrentPosition,
+        calculateClosestPosition,
+        calculateClosestLeftPosition,
+        calculateClosestRightPosition,
         onItemLayout,
     };
 }
 
-export function Highlights() {
-    const currentPosition = useSharedValue(0);
+// @inline
+const ADJUST_NONE = 0;
+// @inline
+const ADJUST_LEFT = 1;
+// @inline
+const ADJUST_RIGHT = 2;
 
-    const [index, setIndex] = React.useState(0);
+function rotateDots(slots: number[], activeIndex: number, direction: number) {
+    'worklet';
+
+    const tempDots = [...slots];
+    let index = activeIndex;
+
+    if (direction < 0) {
+        index -= 1;
+        if (index < 1) {
+            index = 1;
+            const last = tempDots.pop();
+            tempDots.unshift(last as number);
+        }
+    } else {
+        index += 1;
+        if (index > tempDots.length - 2) {
+            index = tempDots.length - 2;
+            const first = tempDots.shift();
+            tempDots.push(first as number);
+        }
+    }
+
+    return {
+        slots: tempDots,
+        activeIndex: index,
+    };
+}
+
+export function Highlights() {
+    const prevPosition = useSharedValue(-1);
+    const currentPosition = useSharedValue(0);
 
     useAnimatedReaction(
         () => currentPosition.value,
         (cur, prev) => {
-            if (cur === prev) {
+            if (prev == null || cur === prev) {
                 return;
             }
-            runOnJS(setIndex)(cur);
+            prevPosition.value = prev;
         },
     );
 
-    const { onItemLayout, calculateCurrentPosition } = usePositions(10);
+    const currentProgress = useSharedValue(0);
+
+    const [index, setIndex] = React.useState(0);
+
+    const prevDotsContext = useSharedValue({
+        slots: [1, 2, 3, 4, 5],
+        activeIndex: 1,
+    });
+    const dotsContext = useSharedValue({
+        slots: [1, 2, 3, 4, 5],
+        activeIndex: 1,
+    });
+    const dotsIndexes = useDerivedValue(() => {
+        // if (prevPosition.value > currentPosition.value && currentProgress.value > 0) {
+        //     return prevDotsContext.value.slots;
+        // }
+        return dotsContext.value.slots;
+    });
+    const activeDotIndex = useDerivedValue(() => {
+        if (prevPosition.value > currentPosition.value && currentProgress.value > 0) {
+            return dotsContext.value.activeIndex + 1;
+        }
+        return dotsContext.value.activeIndex;
+    });
+    const dotProgress = useDerivedValue(() => {
+        let progress = 0;
+
+        if (prevPosition.value > currentPosition.value && dotsContext.value.activeIndex === 1) {
+            progress = currentProgress.value;
+        }
+        if (prevPosition.value < currentPosition.value && dotsContext.value.activeIndex === 3) {
+            progress = currentProgress.value;
+        }
+
+        return progress;
+    });
+
+    useAnimatedReaction(
+        () => currentPosition.value,
+        (cur, prev) => {
+            if (cur === prev || prev == null) {
+                return;
+            }
+            runOnJS(setIndex)(cur);
+            const direction = cur - prev;
+            const newDotsContext = rotateDots(
+                dotsContext.value.slots,
+                dotsContext.value.activeIndex,
+                direction,
+            );
+            prevDotsContext.value = dotsContext.value;
+            dotsContext.value = newDotsContext;
+        },
+    );
+
+    const {
+        onItemLayout,
+        calculateCurrentPosition,
+        calculateClosestPosition,
+        calculateClosestLeftPosition,
+        calculateClosestRightPosition,
+    } = usePositions(10);
+
+    const scrollRef = useAnimatedRef();
 
     const scrollHandler = useAnimatedScrollHandler({
-        onScroll(event, _context) {
+        onScroll(event) {
             const {
                 contentOffset: { x },
             } = event;
 
-            if (x == null) {
-                return;
-            }
-            if (isNaN(x)) {
+            if (x == null || isNaN(x)) {
                 return;
             }
 
-            currentPosition.value = calculateCurrentPosition(x);
+            const positionProgress = calculateCurrentPosition(x);
+            const position = Math.trunc(positionProgress);
+            currentPosition.value = position;
+            currentProgress.value = positionProgress - position;
+        },
+        onEndDrag(event, ctx) {
+            const {
+                contentOffset: { x },
+                velocity,
+            } = event;
+
+            if (velocity != null) {
+                if (velocity.x > 0) {
+                    ctx.adjustOnMomentum = ADJUST_RIGHT;
+                    return;
+                }
+                if (velocity.x < 0) {
+                    ctx.adjustOnMomentum = ADJUST_LEFT;
+                    return;
+                }
+            }
+
+            ctx.adjustOnMomentum = ADJUST_NONE;
+
+            if (x == null || isNaN(x)) {
+                return;
+            }
+
+            const closestX = calculateClosestPosition(x);
+            scrollTo(scrollRef, closestX, 0, true);
+            // currentPosition.value = calculateCurrentPosition(closestX);
+        },
+        onMomentumBegin(event, ctx) {
+            const {
+                contentOffset: { x },
+            } = event;
+
+            if (ctx.adjustOnMomentum === ADJUST_NONE) {
+                return;
+            }
+
+            if (x == null || isNaN(x)) {
+                return;
+            }
+
+            let closestX = 0;
+
+            if (ctx.adjustOnMomentum === ADJUST_LEFT) {
+                closestX = calculateClosestLeftPosition(x);
+            } else if (ctx.adjustOnMomentum === ADJUST_RIGHT) {
+                closestX = calculateClosestRightPosition(x);
+            } else {
+                closestX = calculateClosestPosition(x);
+            }
+
+            scrollTo(scrollRef, closestX, 0, true);
+            // currentPosition.value = calculateCurrentPosition(closestX);
         },
     });
+
+    const dotsWrappers = React.useRef<Record<number, ReturnType<typeof useAnimatedStyle>>>({});
+    const dots = React.useRef<Record<number, ReturnType<typeof useAnimatedStyle>>>({});
+    for (let i = 0; i < dotsIndexes.value.length; i += 1) {
+        const id = i + 1;
+        dotsWrappers.current[id] = useAnimatedStyle(() => {
+            const placement = dotsIndexes.value.findIndex(it => it === id) + 1;
+
+            return {
+                transform: [
+                    {
+                        translateX: interpolate(
+                            placement - dotProgress.value,
+                            [0, 1, 2, 3, 4, 5, 6],
+                            [-32, -16, 0, 16, 32, 48, 64],
+                            Extrapolate.CLAMP,
+                        ),
+                    },
+                ],
+            };
+        });
+        dots.current[id] = useAnimatedStyle(() => {
+            const activeId = dotsIndexes.value[activeDotIndex.value];
+            // console.log(id, activeId, dotsIndexes.value, activeId === id);
+
+            return {
+                backgroundColor: activeId === id ? 'white' : 'rgba(0,0,0,.2)',
+            };
+        });
+    }
 
     return (
         <View style={{ position: 'relative' }}>
             <Animated.ScrollView
+                ref={scrollRef}
                 horizontal
                 onScrollBeginDrag={scrollHandler}
                 scrollEventThrottle={16}
@@ -162,9 +403,7 @@ export function Highlights() {
                                 layout: { x },
                             },
                         }) => {
-                            requestAnimationFrame(() => {
-                                onItemLayout(itemIndex, x);
-                            });
+                            onItemLayout(itemIndex, x);
                         }}
                     />
                 ))}
@@ -172,6 +411,53 @@ export function Highlights() {
             <View style={{ position: 'absolute', left: 0, top: 0 }}>
                 <Text>{index}</Text>
             </View>
+            <View style={{ flexDirection: 'row', marginTop: 8 }}>
+                <View
+                    style={{
+                        overflow: 'hidden',
+                        position: 'relative',
+                        borderRadius: 12,
+                        backgroundColor: 'grey',
+                        width: 16 * 3 + 6 * 2,
+                        height: 24,
+                        paddingHorizontal: 6,
+                        flexDirection: 'row',
+                    }}
+                >
+                    <Animated.View style={[styles.dotWrapper, dotsWrappers.current[1]]}>
+                        <Animated.View style={[styles.dot, dots.current[1]]} />
+                    </Animated.View>
+                    <Animated.View style={[styles.dotWrapper, dotsWrappers.current[2]]}>
+                        <Animated.View style={[styles.dot, dots.current[2]]} />
+                    </Animated.View>
+                    <Animated.View style={[styles.dotWrapper, dotsWrappers.current[3]]}>
+                        <Animated.View style={[styles.dot, dots.current[3]]} />
+                    </Animated.View>
+                    <Animated.View style={[styles.dotWrapper, dotsWrappers.current[4]]}>
+                        <Animated.View style={[styles.dot, dots.current[4]]} />
+                    </Animated.View>
+                    <Animated.View style={[styles.dotWrapper, dotsWrappers.current[5]]}>
+                        <Animated.View style={[styles.dot, dots.current[5]]} />
+                    </Animated.View>
+                </View>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    dotWrapper: {
+        width: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 4,
+        bottom: 4,
+        left: 6,
+    },
+    dot: {
+        width: 6,
+        height: 6,
+        borderRadius: 3,
+    },
+});
