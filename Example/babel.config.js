2e42892ea0e80801d32422a31d5980d7bdb6779d
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2022-07-20 18:55:14 +0300

    UIAmountInput with new architecture.

--- Example/babel.config.js
+++ Example/babel.config.js
@@ -11,7 +11,7 @@ module.exports = {
                     '_hapticNotification',
                     '_injectInputValue',
                     '_moveInputCaret',
-                    '__uiKitInputManager',
+                    '_uiKitInputController',
                 ],
             },
         ],
--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,7 @@ module.exports = function (api) {
                     '_hapticNotification',
                     '_injectInputValue',
                     '_moveInputCaret',
-                    '__uiKitInputManager'
+                    '_uiKitInputController'
                 ],
             },
         ],
