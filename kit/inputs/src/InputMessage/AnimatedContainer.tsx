682c92ca2c6e1ed7a9674154daa7dfe0b3d28e5f
Anatolii <36547799+SAnatoliiS@users.noreply.github.com>
2023-02-21 10:57:21 +0300

    Animate input message.

--- kit/inputs/src/InputMessage/AnimatedContainer.tsx
+++ kit/inputs/src/InputMessage/AnimatedContainer.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
-import type { StyleProp, ViewStyle } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Animated from 'react-native-reanimated';
+
 import { useAnimatedHeight } from './hooks';
 
 type AnimatedContainerProps = {
     children: React.ReactNode;
-    style?: StyleProp<ViewStyle>;
 };
 
-export function AnimatedContainer({ children, style }: AnimatedContainerProps) {
+export function AnimatedContainer({ children }: AnimatedContainerProps) {
     const { onChildrenLayout, animatedStyle } = useAnimatedHeight();
+
     return (
-        <Animated.View style={[style, animatedStyle]}>
+        <Animated.View style={[styles.container, animatedStyle]}>
             <Animated.View onLayout={onChildrenLayout}>{children}</Animated.View>
         </Animated.View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        overflow: 'hidden',
+    },
+});
--- kit/inputs/src/InputMessage/InputMessageContainer.web.tsx
+++ kit/inputs/src/InputMessage/InputMessageContainer.web.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import { UIPressableArea } from '@tonlabs/uikit.controls';
+
 import type { InputMessageContainerProps } from './types';
 import { AnimatedContainer } from './AnimatedContainer';
 
@@ -10,29 +11,21 @@ export function InputMessageContainer({
     onPress,
 }: InputMessageContainerProps): JSX.Element {
     if (!onPress) {
-        return <AnimatedContainer style={styles.container}>{children}</AnimatedContainer>;
+        return <AnimatedContainer>{children}</AnimatedContainer>;
     }
 
     return (
-        <UIPressableArea
-            onPress={onPress}
-            style={[styles.container, styles.pressableContainer, styles.unSelectableText]}
-        >
+        <UIPressableArea onPress={onPress} style={styles.pressableContainer}>
             <AnimatedContainer>{children}</AnimatedContainer>
         </UIPressableArea>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        overflow: 'hidden',
-    },
     pressableContainer: {
-        // To make the pressable area zoom relative to the center of the text
+        // To make the pressable area zooms relative to the center of the text
         alignSelf: 'flex-start',
-    },
-    unSelectableText: {
-        display: 'flex',
+
         userSelect: 'none',
     },
 });
